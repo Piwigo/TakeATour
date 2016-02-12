84b9a9fd50d51028df6a21bebf51f4544b87c220
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-02-12 06:34:37 +0100

    Update de_DE, thanks to madmas

--- language/de_DE/tour_2_7_0.lang.php
+++ language/de_DE/tour_2_7_0.lang.php
@@ -8,7 +8,7 @@ $lang['2_7_0_title5'] = 'Zeit';
 $lang['2_7_0_title1'] = 'Willkommen bei Piwigo 2.7';
 $lang['2_7_0_title11'] = 'Bis bald!';
 $lang['2_7_0_title2'] = 'Die ersten neuen Funktionen';
-$lang['2_7_0_stp2'] = 'Wie du siehst, wurde ein vorgegebenes Plugin hinzugefügt. Take a Tour. Dieses Plugin hilft dir Piwigo zu entdecken. Hier sind alle verfügbaren Touren aufgelistet. <br> Diese Seite ist in <em> Plugins » Take a Tour</em> verfügbar ';
+$lang['2_7_0_stp2'] = 'Wie du siehst, wurde ein vorgegebenes Plugin hinzugefügt. "Take a Tour". Dieses Plugin hilft dir Piwigo zu entdecken. Hier sind alle verfügbaren Touren aufgelistet. <br> Diese Seite ist in <em> Plugins » Take a Tour</em> verfügbar ';
 $lang['2_7_0_stp11'] = 'Diese Tour ist zu Ende. <p style="text-align:center"> Freue dich über Piwigo 2.7! </p> Wenn dir Piwigo gefällt und du uns unterstützen möchtest, dann kannst du beim übersetzen helfen, etwas Spenden usw.  <a href="http://piwigo.org/basics/contribute" target="_blank"> Klicke hier um uns zu unterstützen </a>';
 $lang['2_7_0_stp1'] = 'Hallo! Ich begleite dich, um Piwigo zu entdecken. Bitte folge meinen Anweisungen und klicke auf Nächste (oder benutze die Pfeile der Tastatur) zum navigieren. Wenn du auf andere Seiten gehst, wirst du danach auf die aktuelle Seite der Tour umgeleitet.';
 $lang['2_7_0_stp9'] = 'Ein Suchfilter wurde der Stapelverarbeitung spendiert. Er verfügt über eine mächtige Syntax um Fotos nach Datum, Schlagworten, Größe, Dateinamen zu suchen. Optionen wie Exakt, Und/Oder, Gruppierung und Exkludieren werden unterstützt. Du kannst später die detaillierte Hilfe hierzu lesen. Die gleiche Syntax gilt für die Schnellsuche auf der öffentlichen Seite im Menü.';
