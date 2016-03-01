0dc8933ad7bf9796296edfe9c6e91d5caeef220f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-01 23:56:06 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = 'Os detalhes do usuário não estão mais abrindo dentro da lista de usuários, você recebe um formulário simples, com foco em um único usuário. <br><br>Piwik 2.8 também corrige alguns bugs como atualização da lista de usuários quando o usuário é editado ou verifica se o endereço de e-mail é inválido .';
+$lang['2_8_0_stp2'] = 'Ao notificar um álbum por e-mail, Piwigo 2.8 lhe dá o poder para selecionar usuários, e não apenas os grupos.';
+$lang['2_8_0_stp3'] = 'Basta dizer, chaves de autenticação significa que seus usuários não terão que se lembrar mais de suas senhas. <br><br>Eles vão acessar seu conteúdo privado, diretamente e de forma segura.';
+$lang['2_8_0_stp4'] = 'Agora é possível repetir a marca d\'água em várias linhas. <br><br>Defina na configuração<em>Y repetir</em> para 1 ou 2 para obter a repetição.';
+$lang['2_8_0_stp5'] = 'Quando você excluir um álbum, algumas fotos podem tornar-se órfãs. Piwik 2.8 torna  isso evidente...';
+$lang['2_8_0_stp6'] = '... e deixa você lhedar com eles em apenas um clique!';
+$lang['2_8_0_title1'] = 'Bem-vindo no Piwik 2.8';
+$lang['2_8_0_title2'] = 'Notificar um usuário';
+$lang['2_8_0_title3'] = 'Chaves de autenticação';
+$lang['2_8_0_title4'] = 'Marcadágua';
+$lang['2_8_0_title5'] = 'Órfãos';
+$lang['2_8_0_title6'] = 'Órfãos';
