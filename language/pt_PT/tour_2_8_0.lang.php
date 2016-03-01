fd432e8bbcb3371ab8bb6994d5e8d174d58e7c09
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-01 23:55:45 +0100

    Update pt_PT, thanks to Speed2010

---
+++ language/pt_PT/tour_2_8_0.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_title6'] = 'Órfãos';
+$lang['2_8_0_title5'] = 'Órfãos';
+$lang['2_8_0_title4'] = 'Marca de água';
+$lang['2_8_0_title3'] = 'Chaves de autenticação';
+$lang['2_8_0_title2'] = 'Notificar um usuário';
+$lang['2_8_0_title1'] = 'Bem-vindo ao Piwigo v2.8';
+$lang['2_8_0_stp2'] = 'Ao notificar um álbum por e-mail, Piwigo 2.8 dá-lhe o poder para selecionar usuários, e não apenas os grupos.';
