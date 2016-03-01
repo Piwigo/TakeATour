31729e58fcff3613bdb9d49e3f1b26447d477cd3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-01 15:32:16 +0100

    Add gl_ES, thanks to damufo

---
+++ language/gl_ES/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Next '] = 'Seguinte';
+$lang['Prev'] = 'Anterior';
+$lang['Privacy'] = 'Intimidade';
+$lang['Start the Tour'] = 'Comezar a presentación';
+$lang['TAT_descrp'] = 'Benvido! Aquí está unha lista de todas as posibilidades dispoñíbeis para axudar a descubrir as posibilidades da sua galería Piwigo.<br><br> Inicia unha presentación abaixo e sigue as instrucións. Fai clic en Seguinte/Anterior (ou usa as frechas do teu teclado) para navegar durante a presentación. Cando estás nunha presentación, se vas a outra páxina da administración, serás redirixido para a páxina da presentación actual. Se estás bloqueado e non podes rematar a presentación, vai a a <em>Engadidos » Ver unha presentación</em> e rematará a presentación.';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Ver unha presentación e descubrir as posibilidades da túa galería Piwigo » Ir ás presentacións dispoñibles';
+$lang['Visit your Piwigo!'] = 'Visita o teu Piwigo!';
+$lang['first_contact_descrp'] = 'Recomendado para principiantes, esta presentación introdúcete ó Piwigo e ás súas posibilidades básicas. Este comezará  amosándoche como engadir fotos, e logo xestionalos. A presentación continúa coa xestión dun álbum e finaliza coa personalización usando a configuración, temas e engadidos.';
+$lang['privacy_descrp'] = 'Esta presentación amosa todos os xeitos para protexer túas fotos desde o acceso público con permisos por álbum e por foto. E acceso de copia usando proteccións da imaxe. ';
+$lang['takeatour_configpage'] = 'Ver unha presentación » Lista de presentacións';
+$lang['2.7 Tour'] = 'Presentación do 2.7';
+$lang['2_7_0_descrp'] = 'Descubre o que hai de novo no Piwigo 2.7';
+$lang['End tour'] = 'Fin da presentación';
+$lang['First Contact'] = 'Primeiro contacto';
