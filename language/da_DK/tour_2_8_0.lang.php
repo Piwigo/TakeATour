11ea10290085021d16226a5f9d29f873781095ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-01 23:56:19 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = 'Brugeroplysninger vises ikke længere inde i brugerlisten; der er en simpel formular, med fokus på den enkelte bruger.<br><br>I Piwigo 2.8 rettes også nogle få fejl, så som opdatering af brugerlisten når en bruger er redigeret samt kontrol af ugyldige mailadresser.';
+$lang['2_8_0_stp2'] = 'Når der gives besked om et album via mail, giver Piwigo 2.8 mulighed for at at vælge brugere, ikke kun grupper.';
+$lang['2_8_0_stp3'] = 'Sagt på en anden måde, betyder autentifikationsnøgler, at ens brugere ikke længere skal huske deres adgangskoder.<br><br>De tilgår ens private indhold direkte og på sikker vis.';
+$lang['2_8_0_stp4'] = 'Det er nu muligt at gentage vandmærket over flere linjer.<br><br>Opsæt <em>Y gentag</em>-indstillingen til 1 eller 2, for at opnå gentagelse.';
+$lang['2_8_0_stp5'] = 'Når et album slettes, kan nogle fotografier blive forældreløse. Piwigo 2.8 gør dem åbenlyse...';
+$lang['2_8_0_stp6'] = '... og giver mulighed for at håndtere dem med et enkelt klik!';
+$lang['2_8_0_title1'] = 'Velkommen i Piwigo 2.8';
+$lang['2_8_0_title2'] = 'Giv en bruger besked';
+$lang['2_8_0_title3'] = 'Autentifikationsnøgler';
+$lang['2_8_0_title4'] = 'Vandmærke';
+$lang['2_8_0_title5'] = 'Forældreløse';
+$lang['2_8_0_title6'] = 'Forældreløse';
