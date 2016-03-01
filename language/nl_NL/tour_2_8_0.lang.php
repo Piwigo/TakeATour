777de094abc3291182175a8989638c5d0a932ba4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-01 23:55:53 +0100

    Update nl_NL, thanks to Kees Hessels

---
+++ language/nl_NL/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp3'] = 'Simpel gezegd, door authenticatie sleutels hoeven uw gebruikers hun wachtwoord niet meer te onthouden.<br><br>Zij zullen uw privé-content direct en veilig benaderen.';
+$lang['2_8_0_stp4'] = 'Het is nu mogelijk om het watermerk op meerdere regels te herhalen.<br><br>Stel de <em>Y repeat</em> instelling op 1 of 2 om het watermerk te herhalen.';
+$lang['2_8_0_stp5'] = 'Wanneer u een album verwijdert, kunnen sommige foto\'s wees geworden. Piwigo 2.8 geeft deze duidelijk aan...';
+$lang['2_8_0_stp6'] = '...en geeft de mogelijkheid voor een oplossing met slechts een klik!';
+$lang['2_8_0_title3'] = 'Authenticatie sleutels';
+$lang['2_8_0_stp1'] = 'Gebruikersgegevens worden niet meer geopend in de gebruikerslijst. U krijgt nu een eenvoudig formulier, gericht op een enkele gebruiker.<br><br>Piwigo 2.8 lost ook een paar bugs op zoals gebruikerslijst-updates  zodra de gebruikersgegevens worden bewerkt en het controleren op ongeldige e-mailadressen.';
+$lang['2_8_0_stp2'] = 'Bij het per e-mail informeren over een album, geeft Piwigo 2.8 u de mogelijkheid om individuele gebruikers, niet alleen groepen te selecteren.';
+$lang['2_8_0_title1'] = 'Welkom bij Piwigo 2.8';
+$lang['2_8_0_title2'] = 'Informeer een gebruiker';
+$lang['2_8_0_title4'] = 'Watermerk';
+$lang['2_8_0_title5'] = 'Wezen';
+$lang['2_8_0_title6'] = 'Wezen';
