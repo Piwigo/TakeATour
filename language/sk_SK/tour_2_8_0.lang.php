bbd4f8b8bf3e1cf2539d05ebd449765c3a171eb5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-02 17:27:54 +0100

    Update sk_SK, thanks to dodo

---
+++ language/sk_SK/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = 'Detaily užívateľa už nie sú prístupné v zozname používateľov, dostanete jednoduchý formulár, ktorý je zameraný na jedného používateľa.<br><br>Piwigo 2.8 tiež opravuje niekoľko chýb, ako je aktualizácia zoznamu používateľov, akonáhle používateľ upraví alebo skontroluje neplatnú e-mailovú adresu.';
+$lang['2_8_0_stp2'] = 'Pri oznamovaní o pridaní albumu prostredníctvom e-mailu, Piwigo 2.8 Vám dáva možnosť vybrať používateľov, nie iba skupiny.';
+$lang['2_8_0_stp3'] = 'Jednoducho povedané, autorizačné kľúče znamenajú pre vaších používateľov, že si nebudú musieť pamätať svoje heslo.<br><br> Budú pristupovať k vlastnému obsahu priamo a bezpečne.';
+$lang['2_8_0_stp4'] = 'Teraz je možné opakovať vodoznak na niekoľkých miestach. <br><br>Nastaviť <em>Y opakovaní</em> nastavenie na 1 alebo 2 pre opakovania.';
+$lang['2_8_0_stp5'] = 'Keď odstránite album, niekoľko fotografií sa môže osamostatniť. Piwigo 2.8 Vás upozorní ...';
+$lang['2_8_0_stp6'] = '... a nechať sa vysporiadať s nimi iba jedným kliknutím!';
+$lang['2_8_0_title1'] = 'Vitajte v Piwigo 2.8';
+$lang['2_8_0_title2'] = 'Informovať používateľa';
+$lang['2_8_0_title3'] = 'Autentifikačné kľúče';
+$lang['2_8_0_title4'] = 'Vodoznak';
+$lang['2_8_0_title5'] = 'Osamotené';
+$lang['2_8_0_title6'] = 'Osamotené';
