787f114356936c505174edfac0659049913063d5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-02 06:19:36 +0100

    Update de_DE, thanks to bigant

---
+++ language/de_DE/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = 'Nutzerdetails öffnen sich nicht mehr innerhalb der Benutzerliste. Es gibt nun eine einfachere Form basierend auf dem einzelnen Benutzer. <br><br>Piwigo 2.8 beinhaltet zusätzlich einige Fehlerbehebungen. Nun wird die Benutzerliste aktualisiert, wenn ein Benutzer geändert wird und E-Mail-Adressen werden auf Gültigkeit überprüft.';
+$lang['2_8_0_stp2'] = 'Mit Piwigo 2.8 können für E-Mail-Benachrichtigungen Benutzer ausgewählt werden und nicht nur Gruppen.';
+$lang['2_8_0_stp3'] = 'Einfach ausgedrückt, mit Authentifizierungsschlüsseln müssen sich Ihre Benutzer keine Passwörter mehr merken. <br><br>Auf private Inhalte kann direkt und sicher zugegriffen werden.';
+$lang['2_8_0_stp4'] = 'Nun ist es möglich, das Wasserzeichen auf mehreren Zeilen zu wiederholen. <br><br>Die <em>Wiederholen</em>-Einstellung auf 1 oder 2 setzen, um diese zu aktivieren.';
+$lang['2_8_0_stp5'] = 'Wenn ein Album gelöscht wird, können einige Fotos als verwaiste Dateien zurückbleiben. Mit Piwigo 2.8 werden diese sichtbar gemacht...';
+$lang['2_8_0_stp6'] = '... und können mit einem Klick bearbeitet werden!';
+$lang['2_8_0_title1'] = 'Willkommen bei Piwigo 2.8';
+$lang['2_8_0_title2'] = 'Einen Benutzer benachrichtigen';
+$lang['2_8_0_title3'] = 'Authentifizierungsschlüssel';
+$lang['2_8_0_title4'] = 'Wasserzeichen';
+$lang['2_8_0_title5'] = 'Verwaiste Dateien';
+$lang['2_8_0_title6'] = 'Verwaiste Dateien';
