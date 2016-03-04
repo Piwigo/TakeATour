e35f0d2269b5212c0f6543a1e7dced2b030181b8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-04 09:00:14 +0100

    Update el_GR, thanks to bas_alba

---
+++ language/el_GR/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp5'] = 'Όταν διαγράφετε ένα λεύκωμα, μερικές φωτογραφίες μπορεί να μείνουν ορφανές. Το Piwigo 2.8 τις καθιστά εμφανείς...';
+$lang['2_8_0_stp6'] = '... και σας επιτρέπουν την αντιμετώπισή τους σε ένα μόνο κλικ!';
+$lang['2_8_0_stp4'] = 'Είναι τώρα δυνατόν να επαναλάβεται το υδατογράφημα σε διαφορετικές γραμμές.<br><br>Ρυθμίστε τη ρύθμιση<em> επανάληψη Y </em>σε 1 ή 2 για να εφαρμόσεται την επανάληψη.';
+$lang['2_8_0_title6'] = 'Ορφανά';
+$lang['2_8_0_title5'] = 'Ορφανά';
+$lang['2_8_0_title4'] = 'Υδατογράφημα';
+$lang['2_8_0_title3'] = 'Τα κλειδιά ελέγχου ταυτότητας';
+$lang['2_8_0_title2'] = 'Notify a user';
+$lang['2_8_0_title1'] = 'Welcome in Piwigo 2.8';
+$lang['2_8_0_stp1'] = 'Τα στοιχεία του χρήστη δεν άνοιγουν πλέον στο εσωτερικό της λίστας χρηστών, μπορείτε να πάρετε μια απλή φόρμα επικεντρωμένη σε ένα μόνο χρήστη.<br><br>Το Piwigo 2.8 διορθώνει επίσης μερικά bugs όπως η λίστας χρηστών ενημερώνεται κάθε φορά που επεξεργάζεται ο χρήστης ή ελέγχεται για μη έγκυρη διεύθυνση email.';
+$lang['2_8_0_stp3'] = 'Απλά, αυτό σημαίνει ότι τα κλεδιά ελέγχου ταυτότητας ότι οι χρήστες σας δεν θα χρειάζται να θυμούνται πια τον κωδικό πρόσβασής τους.<br><br>Θα έχουν έτσι άμεση και ασφαλή πρόσβαση στο ιδιωτικό περιεχόμενό σας.';
+$lang['2_8_0_stp2'] = 'Όταν κοινοποιείται ένα λεύκωμα μέσω e-mail, το Piwigo 2.8 σας δίνει τη δυνατότητα να επιλέξετε χρήστες, όχι μόνο ομάδες.';
