6235629da1e5e1498975e5da7faa06de98cbe2c9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-06 06:43:19 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp6'] = '... e vi semplifica la pulizia in un solo click!';
+$lang['2_8_0_title6'] = 'Orfane';
+$lang['2_8_0_title5'] = 'Orfana';
+$lang['2_8_0_title4'] = 'Filigrana';
+$lang['2_8_0_title3'] = 'Chiavi di autenticazione';
+$lang['2_8_0_title2'] = 'Notificare un utente';
+$lang['2_8_0_title1'] = 'Benvenuti in Piwigo 2.8';
+$lang['2_8_0_stp5'] = 'Quando si elimina un album, alcune foto possono diventare orfane. Piwigo 2.8 li mette in evidenza ...';
+$lang['2_8_0_stp4'] = 'Ora è possibile ripetere il filigrana su diverse linee. <br><br>Impostare la <em>Y ripetizione</em> su 1 o 2 per ottenere la ripetizione.';
+$lang['2_8_0_stp3'] = 'In parole semplici, con la chiavi di autenticazione, gli utenti non dovranno più ricordare le loro password. <br><br>Potranno accedere direttamente e in modo sicuro ai contenuti privati.';
+$lang['2_8_0_stp2'] = 'Nel notificare un album via e-mail, Piwigo 2.8 vi permette di selezionare anche degli utenti, non solo dei gruppi.';
+$lang['2_8_0_stp1'] = 'I dettagli del utente non vengono più visualizzati all\'interno della lista degli utenti, si ottiene un elenco posizionandosi su un singolo utente. <br><br>Piwgo 2.8 corregge anche alcuni bug come l\'aggiornamento dell\'elenco utenti quando un utente viene modificato o controllare un indirizzo Email non valido.';
