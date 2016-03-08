5517610ff7fa8c76accc40a7cda90672bf0baec4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-08 11:14:13 +0100

    Add th_TH, thanks to pradit

---
+++ language/th_TH/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp3'] = 'พูดง่ายๆ ว่า รหัสตรวจสอบหมายถึงผู้ใช้งานเว็บไซต์ของคุณ<br><br>พวกเขาไม่จำเป็นต้องจำรหัสผ่านของพวกเขาอีกต่อไป';
+$lang['2_8_0_stp1'] = 'รายละเอียดผู้ใช้จะไม่เปิดภายในรายการผู้ใช้ คุณจะได้รับรูปแบบที่เรียบง่ายมุ่งเน้นไปที่ผู้ใช้คนเดียว<br><br>Piwigo 2.8 ยังแก้ไขข้อบกพร่องน้อยเช่นการปรับปรุงรายชื่อผู้ใช้เมื่อผู้ใช้มีการแก้ไขหรือตรวจสอบที่อยู่อีเมลที่ไม่ถูกต้อง';
+$lang['2_8_0_stp4'] = 'ตอนนี้มันเป็นไปได้ที่จะทำซ้ำลายน้ำในหลายเส้น <br><br>กำหนดค่า <em>แนวตั้ง</em> ตั้งค่า 1 หรือ 2 เพื่อทำซ้ำ';
+$lang['2_8_0_stp2'] = 'เมื่อคุณเลือกแจ้งเตือนอัลบั้มทางอีเมล Piwigo 2.8 จะช่วยให้คุณมีอำนาจในการเลือกผู้ใช้ ซึ่งไม่ใช่แค่เพียงแต่เฉพาะกลุ่ม';
+$lang['2_8_0_stp5'] = 'เมื่อคุณลบอัลบั้มรูปภาพบางภาพอาจจะกลายเป็นรูปออร์แฟนส์ Piwik 2.8 จะทำให้พวกรูปพวกนั้นกลับมา';
+$lang['2_8_0_stp6'] = '...และนี่จะช่วยให้คุณสามารถจัดการกับมันได้ง่ายๆ เพียงแค่คลิก!';
+$lang['2_8_0_title1'] = 'ยินดีต้อนรับสู่ Piwigo 2.8';
+$lang['2_8_0_title2'] = 'แจ้งผู้ใช้งาน';
+$lang['2_8_0_title3'] = 'รหัสตรวจสอบ';
+$lang['2_8_0_title4'] = 'ภาพลายน้ำ';
+$lang['2_8_0_title5'] = 'ออร์แฟนส์';
+$lang['2_8_0_title6'] = 'ออร์แฟนส์';
