df85b4120b1f012762e588229e230075ba5c8d1d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-09 18:04:35 +0100

    Update sl_SI, thanks to MarkoAm

---
+++ language/sl_SI/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = 'Podrobnosti o članih se ne prikazujejo več v spisku uporabnikov temveč v preprostem obrazcu s fokusom na posameznem članu.<br><br>Piwigo 2.8 odpravlja tudi nekaj hroščev kot naprimer posodobitev spiska članov po tem ko je član urejen ali preverjanje napačnega e-mail naslova.';
+$lang['2_8_0_stp2'] = 'Pri obveščanju o spremembah albuma po e-pošti vam Piwigo 2.8 daje možnost da izbirate med člani in ne zgolj med skupinami.';
+$lang['2_8_0_stp3'] = 'Preprosto povedano, overitveni ključi pomenijo, da si vašim članom ne bo več potrebno zapomniti gesla.<br><br> Do vaših zasebnih vsebin bodo lahko dostopali neposredno in varno.';
+$lang['2_8_0_stp4'] = 'sedaj je vodni žig možno ponavljati v več vrsticah.<br><br>Nastavite  <em>ponavljanje po Y osi</em> na 1 ali 2 da vključite ponavljanje.';
+$lang['2_8_0_stp5'] = 'ko izbrišete album, lahko posamezne slike postanejo sirote. Piwigo 2.8 jih naredi očitne....';
+$lang['2_8_0_stp6'] = '...in vam omogoča da opravljate z njimi s samo enim klikom!';
+$lang['2_8_0_title1'] = 'Dobrodošli v Piwigo 2.8';
+$lang['2_8_0_title2'] = 'Obvesti člana';
+$lang['2_8_0_title3'] = 'Overitveni ključi';
+$lang['2_8_0_title4'] = 'Vodni žig';
+$lang['2_8_0_title5'] = 'Sirote';
+$lang['2_8_0_title6'] = 'Sirote';
