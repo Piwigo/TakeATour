00862dfd66e74d86f39866de5a5e3d307af3b985
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-10 22:51:49 +0100

    Add zh_HK, thanks to hkdigit

---
+++ language/zh_HK/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = '用戶詳細信息中的用戶列表內不再打開，你會得到一個簡單的表單，集中於個別用戶。<br><br>Piwigo 2.8還修復了一些bug，如用戶列表更新一次用戶編輯或校驗電子郵件地址。';
+$lang['2_8_0_stp2'] = '當通過電子郵件通知專輯，Piwigo 2.8給你選擇用戶，不僅是用戶組。。';
+$lang['2_8_0_stp3'] = '簡單地說，認證密鑰意味著你的用戶不必記住自己的密碼了。<br><br>他們將直接和安全地訪問您的私有內容。';
+$lang['2_8_0_stp4'] = '現在可能重複幾行水印。<br><br>只要設置<em>重複</em>為1或2。';
+$lang['2_8_0_stp5'] = '當您刪除相冊，一些照片可能成為孤兒。 Piwigo 2.8使得它們顯而易見。';
+$lang['2_8_0_stp6'] = '......按此處理它們！';
+$lang['2_8_0_title1'] = '歡迎Piwigo 2.8';
+$lang['2_8_0_title2'] = '通知用戶';
+$lang['2_8_0_title3'] = '認證密鑰';
+$lang['2_8_0_title4'] = '水印';
+$lang['2_8_0_title5'] = '孤兒';
+$lang['2_8_0_title6'] = '孤兒';
