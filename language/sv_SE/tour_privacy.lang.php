10647b38a440cf3748b218ec3f95248b894fec7f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-12 18:27:37 +0100

    Update sv_SE, thanks to kabo

---
+++ language/sv_SE/tour_privacy.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['privacy_stp1'] = 'Hej! Jag kommer att vara din guide för att lära dej hur du skyddar dina foton i Piwigo. Följ mina instruktioner och klicka Nästa (eller använd pilarna på tangentbordet) för att navigera. Om du går till en annan administrationssida kommer du att skickas tillbaks till den aktuella sidan av den här rundturen. Om du har fastnat och inte kan komma ur rundturen kan du avsluta den genom att gå till <em>Plugins » Take A Tour</em>.<br />Nu börjar vi!';
+$lang['privacy_title9'] = 'Sekretessnivå';
+$lang['privacy_title8'] = 'Sekretessnivå';
+$lang['privacy_title7'] = 'Sekretessnivå';
+$lang['privacy_title6'] = 'Sekretessnivå';
+$lang['privacy_stp8'] = 'Välj ett eller fler foton...';
+$lang['privacy_title19'] = 'Användare';
+$lang['privacy_title17'] = 'Grupper';
+$lang['privacy_title15'] = 'Tips';
+$lang['privacy_title18'] = 'Grupper';
+$lang['privacy_title20'] = 'Publika foton';
+$lang['privacy_title21'] = 'Publika foton';
+$lang['privacy_title3'] = 'Rättigheter';
+$lang['privacy_title4'] = 'Rättigheter';
+$lang['privacy_title5'] = 'Rättigheter > Grupper';
