b13f1be135553be1804e70bcabf08f5e047be2a7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-04-04 06:14:57 +0200

    Update cs_CZ, thanks to edison23

---
+++ language/cs_CZ/tour_2_8_0.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp3'] = 'Jednoduše řečeno, autentizační klíče umožní vašim uživatelům přistupovat ke svému obsahu přímo a bezpečně, aniž by si museli pamatovat své heslo.';
+$lang['2_8_0_stp2'] = 'V nastavení upozornění na album umožňuje Piwigo 2.8 vybrat jednotlivé uživatele, ne pouze celé skupiny.';
+$lang['2_8_0_stp1'] = 'Detaily uživatele už se neotevírají v seznamu uživatelů, ale v jednoduchém samostatném formuláři zaměřeném na jednoho uživatele.<br><br>Piwigo 2.8 také opravuje několik chyb jako aktualizování seznamu uživatelů poté, co byl uživatel upraven, nebo kontrolu validity e-mailové adresy.';
