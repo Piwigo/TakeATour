26b346f0b11612066ff264d384325c153982a281
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-04-06 21:13:33 +0200

    Update zh_CN, thanks to dennisyan

---
+++ language/zh_CN/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = '用户详情现在不再在用户列表内部打开，而是有一个简单的表格，聚焦单一的用户。<br><br>Piwigo 2.8 同时还修正了一些bug，例如当用户被编辑后更新用户列表或检查无效的email地址。';
+$lang['2_8_0_stp4'] = '现在水印能够重复数行了。<br><br>将 <em>Y轴平铺</em> 设置为1或2，使水印重复平铺。';
+$lang['2_8_0_title5'] = '未关联到相册的图片';
+$lang['2_8_0_title6'] = '未关联到相册的图片';
+$lang['2_8_0_stp6'] = '... 并让您只需轻松一击便可处理它们。';
+$lang['2_8_0_title1'] = '欢迎进入 Piwigo 2.8';
+$lang['2_8_0_stp2'] = '当使用email来通告相册更新时，Piwigo 2.8 使您有权选择用户，而不仅限于选择组。';
+$lang['2_8_0_stp3'] = '简言之，验证密钥的意义是使得用户不再需要记住他们的密码。<br><br>他们将能够直接、安全地访问您的非公开内容。';
+$lang['2_8_0_stp5'] = '当您删除一个相册时，某些图片可能成为未关联到任何相册的孤立图片。Piwigo 2.8 使它们一目了然...';
+$lang['2_8_0_title2'] = '通知一位用户';
+$lang['2_8_0_title3'] = '验证密钥';
+$lang['2_8_0_title4'] = '水印';
