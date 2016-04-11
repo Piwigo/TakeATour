ccfda146293b0fb747fe9e65fe361f5a68a18757
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-04-11 06:08:49 +0200

    Update es_ES, thanks to petaqui

---
+++ language/es_ES/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = 'Los datos de usuario no se van a abrir más dentro de la lista de usuarios, obtendrás un formulario simple, enfocado en un sólo usuario.<br><br>Piwigo 2.8 además arregla varios bugs como actualizar la lista de usuarios una vez que uno sea editado, o comprobar emails inválidos.';
+$lang['2_8_0_stp2'] = 'Cuando se notifique un álbum por email, Piwigo 2.8 da la posibilidad de poder seleccionar usuarios, no sólo grupos.';
+$lang['2_8_0_stp3'] = 'Dicho de forma fácil, las claves de autenticación significan que tus usuarios no necesitarán recordar sus contraseñas nunca más.<br><br>Podrán acceder tu contenido privado de forma directa y segura.';
+$lang['2_8_0_stp4'] = 'Ahora es posible repetir la marca de agua en varias líneas.<br><br>Especifíca la <em>repetición en Y</em> estableciendo 1 o 2 para obtener la repetición.';
+$lang['2_8_0_stp5'] = 'Cuando borras un álbum, algunas fotos pueden volverse huérfanos. Piwigo 2.8 lo hace obvio...';
+$lang['2_8_0_stp6'] = '... ¡y permite lidiar con ellos con un solo clic!';
+$lang['2_8_0_title1'] = 'Bienvenido a Piwigo 2.8';
+$lang['2_8_0_title2'] = 'Notifica a un usuario';
+$lang['2_8_0_title3'] = 'Claves de autenticación';
+$lang['2_8_0_title4'] = 'Marca de agua';
+$lang['2_8_0_title5'] = 'Huérfanas';
+$lang['2_8_0_title6'] = 'Huérfanas';
