950a29cf859224ed51d16df1643f1c386f64f491
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-19 06:19:37 +0200

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/tour_2_8_0.lang.php
+++ language/nl_NL/tour_2_8_0.lang.php
@@ -20,13 +20,13 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['2_8_0_stp3'] = 'Simpel gezegd, door authenticatie sleutels hoeven uw gebruikers hun wachtwoord niet meer te onthouden.<br><br>Zij zullen uw privé-content direct en veilig benaderen.';
-$lang['2_8_0_stp4'] = 'Het is nu mogelijk om het watermerk op meerdere regels te herhalen.<br><br>Stel de <em>Y repeat</em> instelling op 1 of 2 om het watermerk te herhalen.';
-$lang['2_8_0_stp5'] = 'Wanneer u een album verwijdert, kunnen sommige foto\'s wees geworden. Piwigo 2.8 geeft deze duidelijk aan...';
-$lang['2_8_0_stp6'] = '...en geeft de mogelijkheid voor een oplossing met slechts een klik!';
+$lang['2_8_0_stp3'] = 'Simpel gezegd, door authenticatie sleutels hoeven jouw gebruikers hun wachtwoord niet meer te onthouden.<br><br>Zij zullen je privé-inhoud direct en veilig benaderen.';
+$lang['2_8_0_stp4'] = 'Het is nu mogelijk om het watermerk op meerdere regels te herhalen.<br><br>Stel de <em>Y herhaal</em> instelling op 1 of 2 om het watermerk te herhalen.';
+$lang['2_8_0_stp5'] = 'Wanneer je een album verwijdert, kunnen sommige foto\'s wees zijn geworden. Piwigo 2.8 geeft deze duidelijk aan...';
+$lang['2_8_0_stp6'] = '...en geeft de mogelijkheid voor een oplossing met slechts één klik!';
 $lang['2_8_0_title3'] = 'Authenticatie sleutels';
-$lang['2_8_0_stp1'] = 'Gebruikersgegevens worden niet meer geopend in de gebruikerslijst. U krijgt nu een eenvoudig formulier, gericht op een enkele gebruiker.<br><br>Piwigo 2.8 lost ook een paar bugs op zoals gebruikerslijst-updates zodra de gebruikersgegevens worden bewerkt en het controleren op ongeldige e-mailadressen.';
-$lang['2_8_0_stp2'] = 'Bij het per e-mail informeren over een album, geeft Piwigo 2.8 u de mogelijkheid om individuele gebruikers, niet alleen groepen te selecteren.';
+$lang['2_8_0_stp1'] = 'Gebruikersgegevens worden niet meer geopend in de gebruikerslijst. Je krijgt nu een eenvoudig formulier, gericht op één enkele gebruiker.<br><br>Piwigo 2.8 lost ook een paar bugs op zoals gebruikerslijst-updates zodra de gebruikersgegevens worden bewerkt en het controleren op ongeldige e-mailadressen.';
+$lang['2_8_0_stp2'] = 'Bij het per e-mail informeren over een album, geeft Piwigo 2.8 je de mogelijkheid om individuele gebruikers en niet alleen groepen te selecteren.';
 $lang['2_8_0_title1'] = 'Welkom bij Piwigo 2.8';
 $lang['2_8_0_title2'] = 'Informeer een gebruiker';
 $lang['2_8_0_title4'] = 'Watermerk';
