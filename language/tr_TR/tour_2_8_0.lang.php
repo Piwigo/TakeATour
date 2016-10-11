19f1cf785ae35e430a1b549dc02f871b4c8ebfc8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-10-11 06:18:20 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/tour_2_8_0.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = 'Kullanıcı bilgilerini artık kullanıcılar listesi içine açılmıyor, 
+tek bir kullanıcıya odaklanmış basit bir form karşınıza çıkıyor.<br><br>Piwigo 2.8 aynı zamanda kullanıcı geçersiz eposta adresini güncellediği veya kontrol ettiğinde karşılaşılan bir kaç hatayı da giderir.';
+$lang['2_8_0_stp2'] = 'Piwigo 2.8 eposta aracılığı ile bir albüm bilgilendirmesi yaparken sadece grupları değil, kullanıcıları da seçmenize olanak tanır.';
+$lang['2_8_0_stp3'] = 'Basitçe söylemek gerekirse, kimlik doğrulama anahtarları artık kullanıcılarınızın şifre hatırlama zorunluluğunu ortadan kaldırır.<br><br>Kullanıcılarınız özel içeriğe direkt ve güvenli biçimde erişebilir.';
+$lang['2_8_0_stp4'] = 'Artık filigranı bir kaç satırda tekrar etmek mümkün.<br><br><em>Y repeat</em> seçeneğini 1 yada 2 yaparak tekrarlamayı seçebilirsiniz.';
+$lang['2_8_0_stp5'] = 'Bir albümü sildiğinizde bazı fotoğraflar sahipsiz kalabilir. Piwigo 2.8 bunları görünür kılar...';
+$lang['2_8_0_stp6'] = '...ve sadece tek tıklamayla onlarla başa çıkmayı sağlar!';
+$lang['2_8_0_title2'] = 'Bir kullanıcıyı haberdar et';
+$lang['2_8_0_title3'] = 'Kimlik doğrulama anahtarları';
+$lang['2_8_0_title5'] = 'Sahipsiz';
+$lang['2_8_0_title6'] = 'Sahipsiz';
+$lang['2_8_0_title1'] = 'Piwigo 2.8 \'e hoş geldiniz';
+$lang['2_8_0_title4'] = 'Filigran';
