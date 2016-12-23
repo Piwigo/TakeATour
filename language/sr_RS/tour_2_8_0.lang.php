7fa0eec5eabb9f3dac4cf35b4f0ed99d84987b7d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-12-23 05:35:41 +0100

    Update sr_RS, thanks to gpopac

---
+++ language/sr_RS/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp5'] = 'Када обришете албум, неке фотографије ће постати сирочићи. Пивиго 2.8 ће вас о томе обавестити…';
+$lang['2_8_0_stp6'] = '…и омогућиће вам да решите проблем у неколико клика!';
+$lang['2_8_0_title2'] = 'Обевести корисника';
+$lang['2_8_0_title3'] = 'Кључеви за пријаву';
+$lang['2_8_0_title5'] = 'Сирочићи';
+$lang['2_8_0_title6'] = 'Сирочићи';
+$lang['2_8_0_stp2'] = 'Када се шаљу обавештења за албуме преко е-поште, Пивиго 2.8 ће омогућити избор корисника, а не само избор групе.';
+$lang['2_8_0_stp3'] = 'Једноставно речено, са кључевима за пријављивање ваши корисници више неће морати да памте лозинке.<br><br>Они ће моћи да приступају свом садржају директно и сигурно.';
+$lang['2_8_0_stp4'] = 'Сада је могуће понављање воденог жига на сваких пар линија.<br><br>Подесите <em>Понављање по Y оси</em> на 1 или 2 да испробате ову опцију.';
+$lang['2_8_0_stp1'] = 'Детаљи о корисницима се више не отварају унутар списка корисника, већ као једноставна површ за сваког појединачног корисника.<br><br>Пивиго 2.8 долази са исправкама неколицине грешака. Списак корисника се сада ажурира након уређивања корисничког налога, а проверава се и исправност адресе за е-пошту.';
+$lang['2_8_0_title1'] = 'Добродошли у Пивиго 2.8';
+$lang['2_8_0_title4'] = 'Водени жиг';
