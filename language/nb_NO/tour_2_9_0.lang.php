f8c51d2595a695ae86e6b50f567ee8623c5ed240
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-17 06:37:40 +0100

    Update nb_NO, thanks to paulen

---
+++ language/nb_NO/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp1'] = 'Her får du den største endringen på 2,9:utseende! Mørk toptekst, bunntekst og menylinje. Mindre rammer.<br><br>Splitter nye administrasjon hjemmeside, kalt "Dashboard", med store ikoner for rask tilgang til hovedsidene.';
+$lang['2_9_0_stp2'] = 'Versjon 2.9 har nå laget en passord generator direkte på brukers opprettelse skjema.<br><br>Klikk på denne linken og se hva som skjer!';
+$lang['2_9_0_stp3'] = 'Når du sletter et album som inneholder bilder, vil Piwigo nå spørre deg hva du vil gjøre med bildene tilknyttet albumet.<br><br>klikk på den slettede linken for å se dem (du vil da avbryte)';
+$lang['2_9_0_stp4'] = 'Nedlastings link på bilders utgave side, for admins.';
+$lang['2_9_0_stp5'] = 'Finn dupliserte bilder basert på checksum (eksakt matche på fil-innhold)';
+$lang['2_9_0_stp6'] = 'Som standard vil historikken nå åpne på den aktuelle måneds statistikk.';
+$lang['2_9_0_stp7'] = 'The tag manager har nå en klar valgmodus, for å utføre en handling i batch.<br><br>Klikk på linken for å bytte til valgmodus.';
+$lang['2_9_0_title1'] = 'Velkommen til Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Passord generator';
+$lang['2_9_0_title3'] = 'Alternativer på album sletting';
+$lang['2_9_0_title4'] = 'Last ned foto';
+$lang['2_9_0_title5'] = 'Duplikater på sjekksum';
+$lang['2_9_0_title6'] = 'Gjeldende måned historie';
+$lang['2_9_0_title7'] = 'Tag valgmodus';
