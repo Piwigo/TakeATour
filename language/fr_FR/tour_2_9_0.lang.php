a6786606b15682736a0f1d1454470e053ad1ed25
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-17 11:14:39 +0100

    Update fr_FR, thanks to laurent.duretz

---
+++ language/fr_FR/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp5'] = 'Trouvez les doublons de photos à l\'aide de la somme de contrôle (correspondance exacte du contenu du fichier)';
+$lang['2_9_0_title5'] = 'Doublon sur la somme de contrôle';
+$lang['2_9_0_stp7'] = 'Le gestionnaire de tags propose maintenant un mode de sélection, à appliquer sur les actions du lot.<br/><br/>Cliquez sur le lien pour changer le mode de sélection.';
+$lang['2_9_0_stp1'] = 'Ici nous abordons les principaux changements en 2.9 : le design ! En-tête, pied de page et barre de menu sombre. Moins de bordures.<br/><br/>Nouvelle page d\'accueil de l\'administration, appellée "Tableau de bord", avec de larges icônes pour un accès rapide aux pages principales.';
+$lang['2_9_0_title3'] = 'Options de la suppression d\'album';
+$lang['2_9_0_stp4'] = 'Lien de téléchargement sur la page d\'édition de photo, pour les admins.';
+$lang['2_9_0_title7'] = 'Mode sélection de tag';
+$lang['2_9_0_stp3'] = 'A la suppression d\'un album avec des photos, Piwigo vous demande maintenant ce que vous souhaitez faire de celles-ci.<br/><br/> Cliquez sur le lien de suppression pour les voir (vous annulerez)';
+$lang['2_9_0_stp2'] = 'La version 2.9 inclut désormais un générateur de mot de passe dans le formulaire de création d\'utilisateur.<br/><br/>Cliquez sur ce lien et regardez ce qui se passe !';
+$lang['2_9_0_title2'] = 'Générateur de mot de passe';
+$lang['2_9_0_stp6'] = 'Par défaut, l\'historique s\'ouvrira maintenant sur les statistiques du mois courant';
+$lang['2_9_0_title1'] = 'Binevenue sur Piwigo 2.9';
+$lang['2_9_0_title4'] = 'Télécharger la photo';
+$lang['2_9_0_title6'] = 'Historique du mois en cours';
