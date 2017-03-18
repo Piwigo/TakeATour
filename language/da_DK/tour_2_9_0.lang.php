ae63ed1a6b23d1eb459ca5340a92b69687aced63
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-18 09:33:59 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp7'] = 'Taghåndteringen har nu en særlig udvælgelsestilstand, som kan benyttes på batchhandlinger<br><br>Klik på linket for at skifte til udvælgelsestilstand.';
+$lang['2_9_0_title7'] = 'Tag-udvælgelsestilstand';
+$lang['2_9_0_stp1'] = 'Her er den største ændring i 2.9: designet!  Mørk sidetop, sidefod og menubjælke. Færre rammer.<br><br>Helt ny administrationsforside kaldet "Instrumentbræt", med store ikoner, der giver hurtig adgang til de vigtigste sider.';
+$lang['2_9_0_stp2'] = 'Version 2.9 indeholder nu en adgangskodegenerator direkte på brugeroprettelsesformularen.<br><br>Klik på dette link for at se hvad der sker!';
+$lang['2_9_0_stp3'] = 'Ved sletning af et album indeholdende fotografier, spørger Piwigo nu hvad du ønsker at gøre ved de fotografier, som er knyttet til albummet.<br><br>Klik på slet-linket, for at se dem (handlingen afbrydes)';
+$lang['2_9_0_stp4'] = 'Downloadlink på fotoredigeringssiden, for administratorer.';
+$lang['2_9_0_stp5'] = 'Find duplikerede fotografier baseret på kontrolsum (nøjagtig overensstemmelse i filindholdet)';
+$lang['2_9_0_stp6'] = 'Som standard vil historikken nu åbne den aktuelle måneds statistikker.';
+$lang['2_9_0_title1'] = 'Velkommen til Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Adgangskodegenerator';
+$lang['2_9_0_title3'] = 'Valgmulighed ved sletning af album';
+$lang['2_9_0_title4'] = 'Download fotografi';
+$lang['2_9_0_title5'] = 'Duplikater findes via kontrolsum';
+$lang['2_9_0_title6'] = 'Aktuel måneds historik';
