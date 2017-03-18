cc3318c0193ec51dda2e5c165eb9d7d28a8d64c1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-18 09:34:07 +0100

    Update sl_SI, thanks to MarkoAm

---
+++ language/sl_SI/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp1'] = 'Začnimo največjo spremembo pri 2,9: oblikovanje! Temna glava, noga in menijska vrstica. Manj meja. <br> Povsem nova upravljalna domača stran, imenovana "Nadzorna plošča", z velikimi ikonami za hiter dostop do glavnih strani.';
+$lang['2_9_0_stp2'] = 'Različica 2.9 zdaj vdela generator gesel neposredno v obrazec za ustvarjanje uporabnika. <br> Kliknite na povezavo za ta ukrep in videli boste, kaj se bo zgodilo!';
+$lang['2_9_0_stp3'] = 'Pri brisanju albuma, ki vsebuje slike, vas bo Piwigo zdaj vprašal, kaj želite storiti s fotografijami, povezanimi v album. <br> Da bi jih videli, kliknite na povezavo za brisanje (klik boste preklicali)';
+$lang['2_9_0_stp4'] = 'Povezava za prenos na strani za urejanje slik , za admina';
+$lang['2_9_0_stp5'] = 'Iskanje podvojenenih fotografij, ki temeljijo na kontrolni vsoti (natančna ujemanje vsebine datotek)';
+$lang['2_9_0_stp6'] = 'Privzeto je, da se bo zgodovina zdaj odprla na statistiki trenutnega meseca.';
+$lang['2_9_0_stp7'] = 'Upravitelj oznak ima sedaj poseben način izbire, da uporabi ukrepe v seriji. <br> Kliknite na povezavo za preklop v način izbire.';
+$lang['2_9_0_title1'] = 'Dobrodošli v Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Generator gesel';
+$lang['2_9_0_title3'] = 'Generator gesel';
+$lang['2_9_0_title4'] = 'Prenesi fotografijo';
+$lang['2_9_0_title5'] = 'Dvojniki v kontrolni vsoti';
+$lang['2_9_0_title6'] = 'Zgodovina trenutnega meseca';
+$lang['2_9_0_title7'] = 'Način izbire označb';
