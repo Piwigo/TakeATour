c779410c254fb1beff8635a96a5d0d48d48dbd9f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-20 12:34:07 +0100

    Update bg_BG, thanks to turnokopa

---
+++ language/bg_BG/tour_2_8_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = 'Детайли за потребителите вече не се отварят вътре в списъка с потребители, вие отваряте единична форма, фокусирана върху отделния потребител.<br><br>В Piwigo 2.8 са отстранени и стари бъгове.';
+$lang['2_8_0_stp3'] = 'С въвеждането на ключ за достъп, вашите потребители няма да е нужно вече да си помнят паролите.<br><br>Те ще се свързват с частна връзка директно и сигурно.';
+$lang['2_8_0_stp5'] = 'Когато изтриете албум, някои снимки може да останат без такъв. Piwigo 2.8 ви ги показва...';
+$lang['2_8_0_title6'] = 'Без албум';
+$lang['2_8_0_title5'] = 'Без албум';
+$lang['2_8_0_stp4'] = 'Вече е възможно да повторите водният знак няколко пъти.<br><br>Въведете <em>Y повторения</em> 1..2...3 колкото желаете.';
+$lang['2_8_0_stp2'] = 'Когато се правите уведомление по имейл за албум, Piwigo ви дава да изберете и потребители, не само групи. ';
+$lang['2_8_0_stp6'] = '...и нека се договорим с тях само с един клик!';
+$lang['2_8_0_title2'] = 'Уведомяване на потребител';
+$lang['2_8_0_title1'] = 'Добре дошли в Piwigo 2.8';
+$lang['2_8_0_title4'] = 'Водни знаци';
+$lang['2_8_0_title3'] = 'Ключове за удостоверяване';
