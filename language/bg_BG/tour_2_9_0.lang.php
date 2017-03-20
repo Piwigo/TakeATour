adbe806c7dcffee3e6cf1ef74b550081f173efb6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-20 12:34:23 +0100

    Update bg_BG, thanks to turnokopa

---
+++ language/bg_BG/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp1'] = 'Ети ни с голямата промяна на дизайна в 2.9! Тъмна заглавна част, футър и менюбар. По-малко граници.<br><br>Брандирана нова административна страница, наречена "Табло", с големи икони за бърз достъп до много страници.';
+$lang['2_9_0_stp3'] = 'Когато се изтрие в албум съдържанието със снимки, Piwigo сега ще ви пита какво искате да направите със снимките асоциирани с албума.<br><br>Натиснете на линка за проба(може и да отмените).';
+$lang['2_9_0_stp7'] = 'Таг мениджърът има обособен режим на избор.<br><br>Натиснете на линка за да промените режима.';
+$lang['2_9_0_stp2'] = 'Версия 2.9 има вграден генератор на пароли директно във формата за създаване на потребители.<br><br>Натиснете на линка и вижте какво се получава!';
+$lang['2_9_0_stp6'] = 'По подразбиране, история не се показва в статистиката за текущия месец.';
+$lang['2_9_0_stp5'] = 'Намери дублиращи се снимки по контролна сума(съвпадение на еднакво файлово съдържание)';
+$lang['2_9_0_stp4'] = 'Свали линк от страницата за редакция на снимка, за админи.';
+$lang['2_9_0_title7'] = 'Режим за избор на таг-ове';
+$lang['2_9_0_title6'] = 'История на текущия месец';
+$lang['2_9_0_title5'] = 'Дублиране на контролна сума';
+$lang['2_9_0_title4'] = 'Сваляне на снимка';
+$lang['2_9_0_title3'] = 'Опции за изтриване на албум';
+$lang['2_9_0_title2'] = 'Генератор на пароли';
+$lang['2_9_0_title1'] = 'Добре дошли в Piwigo 2.9';
