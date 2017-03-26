b200f3c66d5fc137ca698d64860869863654bcf5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-26 21:33:05 +0200

    Update ro_RO, thanks to enels

---
+++ language/ro_RO/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp4'] = 'Link pentru descărcare de pe pagina de editare foto, pentru admini.';
+$lang['2_9_0_title4'] = 'Descarca fotografia';
+$lang['2_9_0_title7'] = 'Modul de selectare al etichetelor';
+$lang['2_9_0_title5'] = 'Dubluri după sumă de control';
+$lang['2_9_0_stp7'] = 'Managerul de etichete are acum un modul de selecție diferit, pentru a aplica acțiuni în grup. <br> <br> Faceți clic pe link-ul pentru a comuta la modul de selecție.';
+$lang['2_9_0_title3'] = 'Opțiuni la ștergerea albumului ';
+$lang['2_9_0_title6'] = 'Istoricul lunii curente';
+$lang['2_9_0_title2'] = 'Generator de parolă';
+$lang['2_9_0_title1'] = 'Bine ai venit in Piwigo 2.9';
+$lang['2_9_0_stp6'] = 'În mod implicit, istoricul se va deschide acum pe statisticile lunii curente.';
+$lang['2_9_0_stp5'] = 'Găsiți fotografii duplicat pe sumei de control (potrivire exactă pentru conținutul fișierului)';
+$lang['2_9_0_stp3'] = 'La ștergerea unui album care conține fotografii, Piwigo vă va întreba acum ce vrei sa faci cu fotografii asociate albumului. <br> <br> click pe link-ul de ștergere pentru a le vedea (vei anula).';
+$lang['2_9_0_stp2'] = 'Versiunea 2.9 încorporează acum un generator de parole direct  în formularul de creare a utilizatorilor. <br><br> Faceți clic pe acest link și vezi ce se întâmplă!';
+$lang['2_9_0_stp1'] = 'Aici avem cea mai mare schimbare la 2.9: design-ul! Antet închis la culoare, subsol și bara de meniu. Mai puține chenare. <br> <br>O pagina de administrare nouă, numită „Tabloul de bord“, cu pictograme mari pentru acces rapid la paginile principale.';
