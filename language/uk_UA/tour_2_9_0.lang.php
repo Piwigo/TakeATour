3b20e8b74b9cd5e48b2f0384c2944a84d7c600ff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-26 21:33:13 +0200

    Update uk_UA, thanks to StreamThreader

---
+++ language/uk_UA/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp4'] = 'Посилання "завантажити" на сторінці редагування фотограції, для адміністраторів';
+$lang['2_9_0_stp1'] = 'Тут ми бачемо найбільші зміни у версіі 2.9: дизайн! Темний заголовок, нижній колонтитул та панель меню. Меньше границь.<br><br>Оновлена домашня сторінка адміністрування, яка зветься "Панель приладів", з великими іконками для швидкого доступу до основних сторінок.';
+$lang['2_9_0_stp2'] = 'Версія 2.9 має вбудований генератор паролів у сторінці створення користувача.<br><br>Натисніть на це посилання та побачите шо трапиться!';
+$lang['2_9_0_stp3'] = 'Коли видаляється альбом в якому є фотографії, Piwigo задасть питання що робити з фотографіями які асоційовані з цим альбомом.<br><br>Настиність на посилання Видалити щоб побачити його (можна скасувати).';
+$lang['2_9_0_stp7'] = 'Менеджер Тегів тепер має окремий режим вибору, для застосування дій в пакетній обробці.<br><br>Натисніть на посилання для переключення в режим вибору.';
+$lang['2_9_0_stp5'] = 'Знайти дублікати фотографій по контольним сумам (точний збіг по вмісту файлу)';
+$lang['2_9_0_stp6'] = 'За замовченням, зараз буде відкрита історія статистики за поточний масяць';
+$lang['2_9_0_title1'] = 'Ласкаво просимо до Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Генератор паролів';
+$lang['2_9_0_title3'] = 'Параметри видалення альбому';
+$lang['2_9_0_title4'] = 'Завантажити фотографію';
+$lang['2_9_0_title7'] = 'Режим вибору за Тегами';
+$lang['2_9_0_title6'] = 'Історія за поточний місяць';
+$lang['2_9_0_title5'] = 'Дублікати за контрольною сумою';
