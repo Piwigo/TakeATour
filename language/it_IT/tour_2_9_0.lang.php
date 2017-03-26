bca5532c980fa3d3ca14c61e1a7f8992ca2ab05d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-26 21:32:57 +0200

    Update it_IT, thanks to rio

---
+++ language/it_IT/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp1'] = 'Qui analizzeremo i grandi cambiamenti della 2.9: il design ! Intestazione, piè di pagina e barra dei menu Dark. Meno bordi. <br/> Nuova homepage per l\'amministrazione, denominato "Quadro di controllo", con grandi icone per l\'accesso rapido alle pagine principali.';
+$lang['2_9_0_stp2'] = 'La versione 2.9 include ora un generatore di password nel modulo di creazione dell\'utente.<br/><br/>Cliccate sul link e guardate ciò che succede!';
+$lang['2_9_0_stp3'] = 'Quando si cancella un album con delle foto, ora Piwigo vi chiede cosa volete farne.<br/><br/>Clicca sul link per cancellare e vedere le foto (potrete sepre annullare)';
+$lang['2_9_0_stp4'] = 'Link di download sulla pagina di modifica, per gli amministratori.';
+$lang['2_9_0_stp5'] = 'Trovare le foto duplicate con il checksum (corrispondenza esatta del contenuto dei file)';
+$lang['2_9_0_stp6'] = 'Di default, la cronologia verra aperta sulle statistiche del mese corrente';
+$lang['2_9_0_stp7'] = 'La gestione dei Tag offre ora una modalità di selezione, da applicare alle azioni per lotti. <br/> Clicca sul link per cambiare la modalità di selezione.';
+$lang['2_9_0_title1'] = 'Benvenuti in Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Generatore di password';
+$lang['2_9_0_title3'] = 'Opzioni per la cancellazione degli album';
+$lang['2_9_0_title4'] = 'Scaricare foto';
+$lang['2_9_0_title5'] = 'Doppione sul checksum';
+$lang['2_9_0_title6'] = 'Cronologia mese corrente';
+$lang['2_9_0_title7'] = 'Modalità di selezione dei Tag';
