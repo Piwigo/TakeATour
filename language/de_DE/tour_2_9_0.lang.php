063ce5db8435221676772712aa025cf5079fcf21
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-27 06:26:27 +0200

    Update de_DE, thanks to Joergen

---
+++ language/de_DE/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp1'] = 'Hier sind die größten Veränderung, an Version 2.9: Design! Dunkle Kopfzeile, Fußzeile und Menüleiste. Weniger Ränder. <br> Brand Neue Administrations-Startseite, genannt „Dashboard“, mit großen Symbolen für den schnellen Zugriff auf die Hauptseiten.';
+$lang['2_9_0_stp2'] = 'Version 2.9 enthält jetzt einen Passwort Generator auf dem "Benutzer Erstellen" erstellen Formular. <br><br> Klicke auf den Aktion Link und sehe was passiert!';
+$lang['2_9_0_stp3'] = 'Wenn ein zu löschendes Album Fotos enthält, wird Piwigo jetzt fragen was Sie mit den Fotos zu dem Album machen wollen. <br> Klicken Sie auf den Link zum löschen, sie zu sehen (Sie werden abbrechen).';
+$lang['2_9_0_stp4'] = 'Download-Link zur Bildbearbeitungsseite für Administratoren.';
+$lang['2_9_0_stp5'] = 'Finden Sie doppelte Fotos anhand von Prüfsummen (exakt dem Dateiinhalt entsprechen)';
+$lang['2_9_0_stp6'] = 'Standardmäßig wird die Entwicklung nur die aktuellen Monat Statistik öffnen.';
+$lang['2_9_0_stp7'] = 'Der Tag-Manager hat jetzt einen eigenen Auswahlmodus, um Aktionen in der Batch-Anwendung auszuführen. <br> Klicke auf den Link um in den Auswahlmodus zu wechseln.';
+$lang['2_9_0_title1'] = 'Willkommen in Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Passwort Generator';
+$lang['2_9_0_title3'] = 'Optionen zur Album Löschung';
+$lang['2_9_0_title4'] = 'Foto runter laden';
+$lang['2_9_0_title5'] = 'Dupliziert eine Prüfsumme';
+$lang['2_9_0_title6'] = 'Aktuelle Monats Entwicklung';
+$lang['2_9_0_title7'] = 'Tag Auswahlmodus ';
