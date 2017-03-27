dc3e5081fdfd65c4eebb67f1c5d22cb3e8c12775
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-27 06:26:10 +0200

    Add mn_MN, thanks to Ulzii

---
+++ language/mn_MN/tour_2_8_0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp4'] = 'Усан хээг хэд хэдэн мөрөнд давтах боломжтой боллоо.<br><br>Давтах тохиргооны тоогоо <em>Y давтах</em> хэсэгт 1 эсвэл 2 г.м. оруулаарай.';
+$lang['2_8_0_title2'] = 'Хэрэглэгчдэд мэдэгдэх';
+$lang['2_8_0_title1'] = 'Piwigo 2.8-д тавтай морил';
+$lang['2_8_0_title4'] = 'Усан хээ';
