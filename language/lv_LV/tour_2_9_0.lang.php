d13571a5ed098f3b16442644a81acbefa000c128
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-04-09 21:53:50 +0200

    Update lv_LV, thanks to agrisans

---
+++ language/lv_LV/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp3'] = 'Dzēšot albumu, kas satur fotogrāfijas, Piwigo tagad jautās jums, ko vēlaties darīt ar fotogrāfijām, kas saistītas ar šo alb;umu.<br><br> Uzklikšķiniet uz dzēst saites, lai tās redzētu (varēsie atcelt).';
+$lang['2_9_0_stp1'] = 'Šeit nu mēs esam ar lielākajām 2.9 versijas izmaiņām: dizains! Tumšas galvenes, kājenes un izvēlnes joslas. Mazākas apmales.<br><br>Jauna administrēšanas mājas lapa, ko sauc par "Dashboard", ar lielām ikonām, lai ātri piekļūtu galvenajām lapām.';
+$lang['2_9_0_stp7'] = 'Tagu pārvaldnieks patlaban ir atšķirīgas atlases režīmā, lai piemērotu darbības sērijā.<br><br>Uzklikšķiniet uz saites, lai pārslēgtos uz atlases režīmu.';
+$lang['2_9_0_stp6'] = 'Pēc noklusējuma tagad tiks atvērta tekošā mēneša statistikas vēsture.';
+$lang['2_9_0_title5'] = 'Dublikāti pēc kontrolsummas';
+$lang['2_9_0_title7'] = 'Tagu atlases režīms';
+$lang['2_9_0_stp2'] = 'Versija 2.9 tagad sevī ietver paroles ģeneratoru tieši lietotāja izveides lapā..<br><br>Noklikšķiniet uz šīs saites un redzēs, kas notiek!';
+$lang['2_9_0_stp5'] = 'Atrast dublikāt fotogrāfijas, pamatojoties uz kontrolsummas (precīza atbilstība pēc faila satura)';
+$lang['2_9_0_title6'] = 'Tekošā mēneša vēsture';
+$lang['2_9_0_title3'] = 'Albūma dzēšanas opcijas';
+$lang['2_9_0_title4'] = 'Lejupielādēt foto';
+$lang['2_9_0_title1'] = 'Laipni lūgti Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Paroles ģenerators';
+$lang['2_9_0_stp4'] = 'Lejupielādes saite uz foto rediģēšanas lapu administratoriem.';
