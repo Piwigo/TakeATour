eca2e771f416bfef3ea696854d4b88c5bcc1bbd3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-04-11 20:28:43 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp2'] = 'Versie 2.9 heeft nu, meteen op het gebruikers aanmeldformulier, een ingebouwde wachtwoordmaker.<br><br>Klik op deze link en bekijk wat er gebeurt!';
+$lang['2_9_0_stp3'] = 'Als je een album met foto’s wilt verwijderen vraagt Piwigo je nu wat je wilt doen met foto’s die verbonden zijn met dit album.<br><br>Klik op de verwijderlink om ze te bekijken (je gaat annuleren).';
+$lang['2_9_0_stp4'] = 'Voor beheerders een download-link op het fotobewerkings-scherm.';
+$lang['2_9_0_stp5'] = 'Zoek foto’s gebaseerd op controlegetal (exacte match met bestandsinhoud).';
+$lang['2_9_0_stp6'] = 'Geschiedenis opent nu standaard met statistieken van de huidige maand. ';
+$lang['2_9_0_stp7'] = 'De label-manager heeft nu een duidelijke selecteer methode om batch-handelingen toe te passen.<br><br>Klik op de link om naar de methode van selecteren te gaan.';
+$lang['2_9_0_title1'] = 'Welkom bij Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Wachtwoordmaker';
+$lang['2_9_0_title3'] = 'Manieren om een album te verwijderen ';
+$lang['2_9_0_title4'] = 'Download foto';
+$lang['2_9_0_title5'] = 'Kopieën op controlegetal';
+$lang['2_9_0_title6'] = 'Huidige maand-geschiedenis';
+$lang['2_9_0_title7'] = 'Selecteer-methode labels';
+$lang['2_9_0_stp1'] = 'We starten met de grootste verandering in 2.9: opmaak! Donkere kopregel, voetregel en menubalk. Minder randen.<br><br>Splinternieuwe startpagina beheer, met de naam “Dashboard”, met grote pictogrammen voor snelle toegang tot hoofdpagina’s.';
