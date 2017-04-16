74b14163ee3e1cab8a9f21044b58a425e33b3f00
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-04-16 08:03:11 +0200

    Update sv_SE, thanks to Elger

---
+++ language/sv_SE/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp6'] = 'Som standard kommer historiken att öppna aktuell månads statistik.';
+$lang['2_9_0_stp7'] = 'Etiketthanteraren har nu ett tydligt markeringsläge för att tillämpa åtgärder i grupp (batch).<br><br>Klicka på länken för att växla till väljarläget.';
+$lang['2_9_0_title1'] = 'Välkommen till Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Lösenordsgenerator';
+$lang['2_9_0_title3'] = 'Alternativ för radering av album';
+$lang['2_9_0_title4'] = 'Ladda ner bild';
+$lang['2_9_0_title5'] = 'Dubletter på kontrollsumma';
+$lang['2_9_0_title6'] = 'Nuvarande månads historia';
+$lang['2_9_0_title7'] = 'Etikettvalsläge';
+$lang['2_9_0_stp1'] = 'Nu kör vi, med den största förändringen på 2,9: design! Mörkt sidhuvud, sidfot och menyrade. Mindre ramar. <br> Helt ny administrationshemsida, kallad "Dashboard", med stora ikoner för snabb åtkomst till huvudsidorna .';
+$lang['2_9_0_stp2'] = 'Version 2.9 bäddar nu in en lösenordsgenetator direkt i skapa användare formuläret.<br><br>Klicka på den här länken för att se vad  som händer!';
+$lang['2_9_0_stp3'] = 'När man raderar ett album som innehåller, kommer Piwigo fråga dig vad du vill ska hända med bilderna som är kopplade till albumet.<br><br>Klicka på radera-länken för att se dem (du kommer att avbryta).';
+$lang['2_9_0_stp4'] = 'Nedladdningslänk på bildredigeringssidan, för admins.';
+$lang['2_9_0_stp5'] = 'Hitta dubletter baserade på checksumma (exakt matchning på filinnehåll).';
