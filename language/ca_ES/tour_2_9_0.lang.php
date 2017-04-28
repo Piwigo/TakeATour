aec133e75ff5437bc9281d53b97e243b98d16b42
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-04-28 20:31:38 +0200

    Update ca_ES, thanks to josep.torne

---
+++ language/ca_ES/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp7'] = 'Ara. el gestor d\'etiquetes té un mode de selecció diferent per aplicar les accions en mode fitxer de lot. <br><br>Feu clic sobre l\'enllaç per a canviar el mode de selecció. ';
+$lang['2_9_0_stp1'] = 'Aquí estem amb el canvi més gran de la versió 2.9! Capçalera negra, peu de pàgina i barra de menú. Marges més petits. <br><br> Una nova pàgina d\'administració, anomenada "Panell de control", amb icones grans per un accés ràpid a les pàgines principals.';
+$lang['2_9_0_stp2'] = 'Ara la versió 2.9 incorpora un generador de contrasenyes en el formulari de creació d\'usuaris. <br><br> Feu clic en l\'enllaç i mireu que passa!';
+$lang['2_9_0_stp3'] = 'Quan s\'elimina un àlbum que conté fotos, Piwigo pregunta que voleu fer amb les fotos associaces a l\'àlbum. <br><br> Feu clic a l\'enllaç per veure-les (També podeu cancel·lar).';
+$lang['2_9_0_stp4'] = 'Enllaç de descàrrega en la pàgina d\'edició de la foto, per administradors.';
+$lang['2_9_0_stp6'] = 'Per defecte, l\'historial obre les estadístiques del mes actual.';
+$lang['2_9_0_title3'] = 'Opcions d\'eliminació d\'àlbums';
+$lang['2_9_0_title5'] = 'Duplicats per checksum';
+$lang['2_9_0_title6'] = 'Historial del mes actual';
+$lang['2_9_0_stp5'] = 'Busca fotos duplicades basant-se amb el checksum (contingut exacte  del fitxer)';
+$lang['2_9_0_title1'] = 'Benvingut a Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Generador de contrasenyes';
+$lang['2_9_0_title4'] = 'Descarrega la foto';
+$lang['2_9_0_title7'] = 'Mode de selecció d\'etiquetes';
