f2b00e47b721aac2ead7762e6044721f9d2f3425
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-05-03 06:17:54 +0200

    Add eu_ES, thanks to bixerdo

---
+++ language/eu_ES/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['takeatour_configpage'] = 'Hasi aurkezpena » Aurkezpenen zerrenda';
+$lang['privacy_descrp'] = 'Aurkezpen honek zure argazkiak nola babestu erakutsiko dizu. Sarbide baimenak albumaka eta argazkiko nola ezarri eta kopiak nola saiestu. ';
+$lang['plugins_name'] = 'Pluginak';
+$lang['plugins_descrp'] = 'Pluginak Piwigoren funtsezko ezaugarria dira. Hainbat ezarpen gaitu klik gutxi batzuekin eta egokitu Piwigo zure beharretara.';
+$lang['manage_albums_name'] = 'Kudeatu albumak';
+$lang['manage_albums_descrp'] = 'Albumen hierarkia galeriaren azpiegitura da. Ezagutu nola antolatu eta beraien ezaugarriak aldatzen.';
+$lang['2.7 Tour'] = '2.7 bertsioaren aurkezpena';
+$lang['2.8 Tour'] = '2.8 bertsioaren aurkezpena';
+$lang['TAT_descrp'] = 'Ongi etorri! Hona hemen Piwigo galeriaren ezaugarriak ezagutzen lagunduko dizkizuten aurkezpenen zerrenda.<br><br>Abiarazi aurkezpenen bat eta Hurrengoa/Aurrekoa sakatu edo teklatuko geziak erabili bertatik nabigatzeko. Aurkezpenean zauden bitartean ezingo duzu administrazio-orririk erabili. Zeure burua aurkezpenen batean trabaturik aurkitzen baldin baduzu joan <em>Pluginak » Hasi aurkezpena</em>ra eta aurkezpena berehala gelditu beharko litzateke.';
+$lang['first_contact_descrp'] = 'Hasiberrientzat gomendatuta. Aurkezpen honek Piwigoren pausurik onarrizkoenak aurkezten dizkizu: album bat sortzea eta lehen argazkiak igotzea.';
+$lang['edit_photos_name'] = 'Editatu argazkiak';
+$lang['edit_photos_descrp'] = 'Argazkiak dira Piwigoren osagairik onarrizkoenak. Ikusi nola alda ditzakezu beraien ezaugarriak banaka zein multzoka.';
+$lang['config_name'] = 'Konfigurazioa';
+$lang['config_descrp'] = 'Konfiguratu Piwigoren ezarpenak. Aurkitu gaien kudeatzailea eta hautatu zure Piwigoren itxura.';
+$lang['Visit your Piwigo!'] = 'Bisitatu zure Piwigo!';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Hasi aurkezpena eta aurkitu Piwigo galeriaren funtzioak » Aurkezpen erabilgarriak';
+$lang['2_9_0_descrp'] = 'Aurkitu Piwigo 2.9ren ezaugarri berriak. Ezagutu xehetasunak <a href="%s">bertsioaren oharretan</a>.';
+$lang['2_9_0_name'] = '2.9 bertsioaren aurkezpena';
+$lang['End tour'] = 'Amaitu aurkezpena';
+$lang['First Contact'] = 'Lehenengo kontaktua';
+$lang['Next '] = 'Hurrengoa';
+$lang['Prev'] = 'Aurrekoa';
+$lang['Privacy'] = 'Pribatutasuna';
+$lang['Start the Tour'] = 'Hasi aurkezpena';
+$lang['2_8_0_descrp'] = 'Aurkitu Piwigo 2.8ren ezaugarri berriak. Ezagutu xehetasunak <a href="%s">bertsioaren oharretan</a>.';
+$lang['2_7_0_descrp'] = 'Aurkitu zer dagoen berri Piwigo 2.7an';
