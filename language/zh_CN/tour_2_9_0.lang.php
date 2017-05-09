6ad517422a73c7caefe866d687b888828e465845
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-05-09 06:49:22 +0200

    Update zh_CN, thanks to dennisyan

---
+++ language/zh_CN/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp1'] = '让我们来看看 V 2.9 的最大改变：设计！深灰色的页眉、页脚和菜单栏。更少的边框。<br><br>全新的管理界面首页，叫作“仪表板”，有着能够快速链接到主要页面的大图标。';
+$lang['2_9_0_stp2'] = 'V 2.9 现在在新建用户表单中直接嵌入了一个密码生成器。<br><br>点击这个操作链接，看看会发生什么！';
+$lang['2_9_0_stp3'] = '现在，在删除包含图片的相册时，Piwigo 会询问你如何处置关联到相册的图片。<br><br>点击删除链接以查看它们（可以取消）。';
+$lang['2_9_0_stp4'] = '图片编辑页上为管理员提供的下载链接。';
+$lang['2_9_0_title5'] = '校验和查重';
+$lang['2_9_0_stp5'] = '通过校验和（文件内容的精确匹配）查找重复图片';
+$lang['2_9_0_stp7'] = '现在，标签管理有了一个明确的选择模式。<br><br>点击链接以切换到选择模式。';
+$lang['2_9_0_stp6'] = '现在，在默认情况下，历史记录将打开为本月统计信息。';
+$lang['2_9_0_title7'] = '标签选择模式';
+$lang['2_9_0_title6'] = '本月历史记录';
+$lang['2_9_0_title4'] = '下载照片';
+$lang['2_9_0_title3'] = '相册删除选项';
+$lang['2_9_0_title2'] = '密码生成器';
+$lang['2_9_0_title1'] = '欢迎来到 Piwigo 2.9';
