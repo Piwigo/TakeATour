01d62bcfc5c4adedfed95853fe7eae5e12063841
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-05-30 06:20:51 +0200

    Add en_GB, thanks to Pat2309

---
+++ language/en_GB/tour_first_contact.lang.php
@@ -0,0 +1,70 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_title3'] = 'Add Photo Page';
+$lang['first_contact_title33'] = 'Guest settings';
+$lang['first_contact_title34'] = 'Themes';
+$lang['first_contact_title35'] = 'Themes';
+$lang['first_contact_title37'] = 'Themes';
+$lang['first_contact_title38'] = 'Plugins';
+$lang['first_contact_title39'] = 'Plugins';
+$lang['first_contact_title4'] = 'Let\'s add a photo!';
+$lang['first_contact_title43'] = 'It\'s been a great time';
+$lang['first_contact_title9'] = 'The Batch Manager';
+$lang['first_contact_stp4'] = 'First choose an album; create one if there is no album yet.';
+$lang['first_contact_stp40'] = 'It\'s me here! Don\'t deactivate me now, but you can see you can Deactivate or Restore an activated plugin.';
+$lang['first_contact_stp41'] = 'You can see on the last tab all the plugins that are available for your version of Piwigo. Hundred of plugins are there!';
+$lang['first_contact_title15'] = 'The edit page of a photo';
+$lang['first_contact_title18'] = 'Manage albums';
+$lang['first_contact_title19'] = 'Manage Albums';
+$lang['first_contact_title20'] = 'Manage Albums';
+$lang['first_contact_title21'] = 'Other Tabs';
+$lang['first_contact_title22'] = 'Album Edit Page';
+$lang['first_contact_title28'] = 'Configuration';
+$lang['first_contact_stp30'] = 'Let\'s start by changing the title';
+$lang['first_contact_stp32'] = 'Now save your changes';
+$lang['first_contact_stp35'] = 'Set your favorite theme as the default theme.';
+$lang['first_contact_stp5'] = 'Then click the button to select photos to send, or just drag and drop your files.';
+$lang['first_contact_stp6'] = 'You see that photos are being listed. When you\'re ready click the button Start Upload';
+$lang['first_contact_stp8'] = 'Let\'s edit them! Click on the link.';
+$lang['first_contact_title1'] = 'Welcome in your Piwigo gallery';
+$lang['first_contact_title11'] = 'How to edit a photo';
+$lang['first_contact_stp10'] = 'Here is the link to directly access your caddie: that\'s a tool for administrators to easily pick photos when browsing the public part in order to edit them in the Batch Manager.<br><br>You can add photos to the caddie from the public photo page when logged in as an admin, or using the action "Add to caddie" on the Batch Manager.';
+$lang['first_contact_stp11'] = 'From here you can select one or more photos';
+$lang['first_contact_stp12'] = 'then edit them using an action';
+$lang['first_contact_stp13'] = 'You can edit descriptions and more from the batch manager in single mode. But for now, let\'s stay in the global mode';
+$lang['first_contact_stp14'] = 'Now we will see the <b>edit page</b> of one photo. That page is reachable from here but also from the public photo page when logged as an administrator.<br><br>Hover the thumbnail then click on Edit.';
+$lang['first_contact_stp15'] = 'On this page you can edit all the properties of a photo, for instance...';
+$lang['first_contact_stp16'] = '...in which album(s) is the photo.<br><br>So here you can add or remove the photo from albums. The link is virtual, no photos will be physically moved ever.';
+$lang['first_contact_stp17'] = 'Here you can set the photo as a thumbnail of one or several albums. The photo doesn\'t have to be associated with the album to be its thumbnail. If the photo is associated with the album, it\'s even simpler to set it as album thumbnail from the public page of the photo (tip: use plugin Admin Tools for even more simplicity).';
+$lang['first_contact_stp18'] = 'Let\'s see how to manage albums now. Click <em>On album » Manage</em> or hit Next';
+$lang['first_contact_stp19'] = 'Here are listed \'root\' albums. If you see an album with a dashed background, it means a physical album from an FTP synchronization. Physical albums can\'t be moved or deleted from here. The other albums are called virtual albums.';
+$lang['first_contact_stp19_b'] = 'Here are listed \'root\' albums.';
+$lang['first_contact_stp2'] = 'The link is here. Click on it or click Next to continue';
+$lang['first_contact_stp20'] = 'When your mouse hovers over an album, links and information appear. When you drag and drop an album -a rounded block-, you will change its position and then you can save or cancel your manual order.';
+$lang['first_contact_stp21'] = 'The next tab is a page where you can move any virtual album.<br>The permalink tab is to set a particular url for an album.<br>But let\'s edit an album: click on Edit an album';
+$lang['first_contact_stp22'] = 'Like for a photo, here you can edit the properties of an album';
+$lang['first_contact_stp23'] = 'Locking an album means only administrators will be able to see that album: very useful when preparing the album before publishing';
+$lang['first_contact_stp28'] = 'Now we will look at the options available to set Piwigo working your way. Click on Configuration->Options or just hit Next.';
+$lang['first_contact_stp29'] = 'Here, on this first page, you will set the essential and basic configuration of your gallery.';
+$lang['first_contact_stp3'] = 'This first tab is where you add photos directly from your browser. The other tabs are about the other ways to upload photos';
+$lang['first_contact_stp1'] = 'Hello! I will be your guide to discovering Piwigo. Please follow my instructions, and click Next (or use the arrows of your keyboard) to navigate. If you go to another page of the administration, you will be redirected to the current page of the Tour. If you are stuck and can\'t end the tour, going to <em>Plugins » Take A Tour</em> will end the tour.<br>Let\'s start by adding photos!';
