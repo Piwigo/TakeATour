12c195d626f9a88871b6fb1fb9bcc1e1442af62a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-01-27 08:36:50 +0100

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp3'] = 'Przy kasowaniu albumu zawierającego zdjęcia, Piwigo spyta teraz, co chcesz zrobić z powiązanymi zdjęciami.<br><br>Kliknij na link do kasowania, by zobaczyć opcje (później anulujesz operację)';
+$lang['2_9_0_stp7'] = 'Menadżer znaczników posiada teraz specjalny tryb wyboru, by móc wykonywać akcje grupami.<br><br>Kliknij na link, by przejść do trybu wyboru.';
+$lang['2_9_0_stp1'] = 'Najważniejsza zmiana w 2.9: Wygląd! Ciemny nagłówek i stopka, oraz listwa menu. Mniej ramek.<br><br>Zupełnie nowa strona główna dla administratorów, zwana "Pulpitem". Z dużymi ikonami by ułatwić szybki dostęp do podstawowych stron.';
+$lang['2_9_0_stp2'] = 'Wersja 2.9 zawiera generator hasła w formularzu tworzenia użytkownika.<br><br>Naciśnij przycisk i zobacz co się stanie!';
+$lang['2_9_0_stp4'] = 'Link pobierania na stronie edycji zdjęcia, dla administratorów.';
+$lang['2_9_0_stp5'] = 'Znajdź zduplikowane zdjęcia na podstawie sum kontrolnych (dokładnie taka sama zawartość pliku)';
+$lang['2_9_0_stp6'] = 'Domyślnie, historia otworzy się na statystykach bieżącego miesiąca.';
+$lang['2_9_0_title1'] = 'Witamy w Piwigo 2.9';
+$lang['2_9_0_title3'] = 'Opcje kasowania albumu';
+$lang['2_9_0_title2'] = 'Generator hasła';
+$lang['2_9_0_title4'] = 'Pobierz zdjęcie';
+$lang['2_9_0_title5'] = 'Zduplikowane sumy kontrolne';
+$lang['2_9_0_title6'] = 'Historia z bieżącego miesiąca';
+$lang['2_9_0_title7'] = 'Tryb wybierania znaczników';
