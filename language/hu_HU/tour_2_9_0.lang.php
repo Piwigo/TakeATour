cef07619512087bd43f909298c6d16b5e78f87a3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 06:21:12 +0100

    Update hu_HU, thanks to bugge

---
+++ language/hu_HU/tour_2_9_0.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp2'] = 'A 2.9-es verzió tartalmaz jelszó generátort közvetlenül a felhasználó létrehozásánál.<br><br>Kattintson ide az eléréséhez. ';
+$lang['2_9_0_title4'] = 'Kép letöltés';
+$lang['2_9_0_title7'] = 'Címke választó mód';
+$lang['2_9_0_title6'] = 'Az aktuális hónap előzményei';
+$lang['2_9_0_title2'] = 'Jelszó generátor';
+$lang['2_9_0_title1'] = 'Üdvözöllek a Piwigo 2.9-ben. ';
