75933423fc3c2984665fdffe1b1df47dc273dc87
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-31 07:03:57 +0200

    Add lt_LT, thanks to dalius

---
+++ language/lt_LT/tour_2_9_0.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_title3'] = 'Albumo ištrynimo parinktys';
+$lang['2_9_0_stp3'] = 'Kai trinsite albumą su jame esančiomis nuotraukomis, Piwigo jūsų paklaus ką jūs norite daryti su šio albumo nuotraukomis.<br><br>Spustelėkite ant ištrynimo nuorodos norėdami jas pamatyti (jūs galėsite veiksmą atšaukti).';
+$lang['2_9_0_stp4'] = 'atsisiųsti nuorodą nuotraukos koregavimo puslapyje, administratoriams.';
+$lang['2_9_0_stp2'] = 'Dabar 2.9 versijoje slaptažodžių generatorius įterptas tiesiogiai į naudotojo kūrimo formą.<br><br>Paspausk šią nuorodą ir pamatysi kas atsitiks! ';
+$lang['2_9_0_stp6'] = 'Pagal numatytuosius nustatymus, istorija bus atidaryta pagal dabartinio mėnesio statistiką';
+$lang['2_9_0_title6'] = 'Dabartinio mėnesio istorija';
+$lang['2_9_0_title7'] = 'Žymų pasirinkimo rėžimas';
+$lang['2_9_0_title1'] = 'Sveiki atvykę į Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Slaptažodžio generatorius';
+$lang['2_9_0_title4'] = 'Parsisiųsti nuotrauką';
