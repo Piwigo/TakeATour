c2f3c2cb6a46f80e4948ce17584df76bb0b3556c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-31 15:28:58 +0200

    Update pt_PT, thanks to jonxvel

---
+++ language/pt_PT/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp3'] = 'Ao excluir um álbum contendo fotos, o Piwigo agora lhe perguntará o que você quer fazer com as fotos associadas ao álbum. <br><br> Clique no link excluir para vê-las (você vai cancelar).';
+$lang['2_9_0_stp4'] = 'Link de download na página de edição de fotos, para administradores.';
+$lang['2_9_0_stp5'] = 'Localizar fotos duplicadas com base na soma de verificação (correspondência exata no conteúdo do arquivo)';
+$lang['2_9_0_stp6'] = 'Por padrão, o histórico será aberto nas estatísticas do mês atual.';
+$lang['2_9_0_stp7'] = 'O gerenciador de etiquetas agora tem um modo de seleção distinto, para aplicar ações em lote. <br><br> Clique no link para alternar para o modo de seleção.';
+$lang['2_9_0_title1'] = 'Bem-vindo a Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Gerador de senhas';
+$lang['2_9_0_title3'] = 'Opções de eliminação de álbum';
+$lang['2_9_0_title4'] = 'Baixar foto';
+$lang['2_9_0_title5'] = 'Duplicados na soma de verificação';
+$lang['2_9_0_title6'] = 'Histórico do mês atual';
+$lang['2_9_0_title7'] = 'Modo de seleção de etiquetas';
+$lang['2_9_0_stp2'] = 'Versão 2.9 agora incorpora um gerador de senhas diretamente no formulário de criação do usuário. <br><br> Clique neste link de ação e veja o que acontece!';
+$lang['2_9_0_stp1'] = 'Aqui vamos nós com a maior mudança no 2.9: design! Cabeçalho, rodapé e barra de menus escuros. Menos bordas. <br><br> Nova página inicial de administração, chamada "Painel", com ícones grandes para acesso rápido às páginas principais.';
