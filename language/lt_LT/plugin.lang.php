f352d62eb9106e40a726509ab9a0828eba656f89
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-31 07:03:37 +0200

    Add lt_LT, thanks to dalius

---
+++ language/lt_LT/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['manage_albums_name'] = 'Valdykite albumus';
+$lang['edit_photos_descrp'] = 'Nuotraukos yra pagrindinis elementas jūsų kuriamame Piwigo. Žiūrėkite kaip jūs galite keisti jų nustatymus, viena nuotrauka vienu metu arba grupėje';
+$lang['edit_photos_name'] = 'Koreguoti nuotraukas';
+$lang['config_name'] = 'Konfigūravimas';
+$lang['config_descrp'] = 'Redaguokite savo Piwigo nustatymus. Atraskite temų pasirinkimą su " theme manager" ir nustatykite savo Piwigo išvaizdą.';
+$lang['Visit your Piwigo!'] = 'Aplankykite savo Piwigo!';
+$lang['Start the Tour'] = 'Pradėti turą';
+$lang['Privacy'] = 'Privatumas';
+$lang['Next '] = 'sekantis';
+$lang['First Contact'] = 'Pirmas kontaktas';
+$lang['End tour'] = 'Turo pabaiga';
+$lang['2_9_0_name'] = '2.9 turas';
+$lang['2_8_0_descrp'] = 'atraskite keletą naujų Piwigo 2.8 įpatumų. Skaitykite išsamią informaciją <a href="%s">išleidimo užrašai</a>.';
+$lang['2_9_0_descrp'] = 'atraskite keletą naujų Piwigo 2.9 įpatumų. Skaitykite išsamią informaciją <a href="%s">išleidimo užrašai</a>.';
+$lang['2_7_0_descrp'] = 'Atraskite kas naujo versijoje Piwigo 2.7';
+$lang['2.7 Tour'] = '2.7 turas';
+$lang['2.8 Tour'] = '2.8 turas';
