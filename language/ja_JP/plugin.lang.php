cc76f258da8106baa19ce2ddda39c3236d7ef808
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-10 21:00:05 +0200

    Update ja_JP, thanks to Trumpet

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['TAT_descrp'] = 'ようこそ！これはあなたのPiwigoギャラリーの機能をあなたが見つけ出すのを助けるための全ての実行可能なツアー一覧です。<br><br>以下のツアーを起動し、指示に従って下さい。次へ/前へをクリックして（またはキーボードの矢印ーを使って）ツアーを閲覧して下さい。あなたがツアーにいる際にもしあなたが他の管理ページに行こうとしても、あなたはツアーの現在ページヘとリダイレクトされるでしょう。もしあなたが抜け出せずにツアーを終了できなくなったら、<em>プラグイン » ツアーを開始する</em>へ行けばツアーを終えられるでしょう。';
+$lang['privacy_descrp'] = 'このツアーでは、アルバム毎や写真毎の権限設定によりパブリック・アクセスからあなたの写真を保護したり、イメージ保護機能を用いて複製から保護するすべての方法を紹介します。';
+$lang['2_8_0_descrp'] = 'Piwigo 2.8の新機能を見つけるには、<a href="%s">リリースノート</a>を詳しく読んでください。';
+$lang['2_9_0_descrp'] = 'Piwigo 2.9の新機能を見つけるには、<a href="%s">リリースノート</a>を詳しく読んでください。';
+$lang['First Contact'] = '最初の一歩';
+$lang['config_descrp'] = 'あなたのPiwigo設定を行います。テーマ管理を見つけて、あなたのPiwigoの見え方を選択しましょう。';
+$lang['edit_photos_descrp'] = '写真はあなたのPiwigoの基本要素です。それらのプロパティをどのように変更できるか見て下さい、1写真ずつかバッチ処理か。';
+$lang['first_contact_descrp'] = '初心者向け、このツアーはあなたをPiwigoの第一歩へ招待します：アルバムを作成し、あなたの最初の写真を追加します。';
+$lang['manage_albums_descrp'] = 'あなたのアルバム階層が、あなたのギャラリーの構造です。それらを構成したりプロパティを編集する方法を学びましょう。';
+$lang['plugins_descrp'] = 'プラグインはPiwigoの鍵となる機能です。数クリックで様々な機能を追加でき、Piwigoをあなたの要求に合わせることができます。';
+$lang['takeatour_configpage'] = 'ツアーに参加する » ツアーのリスト';
+$lang['Privacy'] = 'プライバシー';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'ツアーに参加してあなたのPiwigoギャラリーの特徴を見つける » 可能なツアーへ行く';
+$lang['manage_albums_name'] = 'アルバムを管理する';
+$lang['plugins_name'] = 'プラグイン';
+$lang['2.7 Tour'] = '2.7ツアー';
+$lang['2.8 Tour'] = '2.8ツアー';
+$lang['2_7_0_descrp'] = 'Piwigo 2.7の新項目を調べる';
+$lang['2_9_0_name'] = '2.9ツアー';
+$lang['End tour'] = 'ツアーを終了する';
+$lang['Next '] = '次へ';
+$lang['Prev'] = '前へ';
+$lang['Start the Tour'] = 'ツアーを開始する';
+$lang['Visit your Piwigo!'] = 'あなたのPiwigoに訪問する！';
+$lang['config_name'] = '設定';
+$lang['edit_photos_name'] = '写真を編集する';
