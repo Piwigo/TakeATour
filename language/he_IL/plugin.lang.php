555dd03b4b52ddc1d5ffc6ccba9db01bf6e8742c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-19 21:17:00 +0200

    Update he_IL, thanks to abenzev

--- language/he_IL/plugin.lang.php
+++ language/he_IL/plugin.lang.php
@@ -21,14 +21,29 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['2.7 Tour'] = 'סיור 2.7';
-$lang['2_7_0_descrp'] = 'גלה מה חדש ב-Piwigo 2.7';
+$lang['2_7_0_descrp'] = 'Piwigo 2.7-גלה מה חדש ב';
 $lang['End tour'] = 'סוף סיור';
 $lang['First Contact'] = 'איש קשר ראשון';
 $lang['Next '] = 'הבא';
-$lang['Prev'] = 'הקודם';
+$lang['Prev'] = 'קודם';
 $lang['Privacy'] = 'פרטיות';
 $lang['Start the Tour'] = 'התחל את הסיור';
-$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'צא לסיור וגלה את הפית׳רים של גלריית Piwigo שלך >> צא לסיורים זמינים';
-$lang['Visit your Piwigo!'] = 'בקר ב-Piwigo שלך!';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'צא לסיור וגלה את הפית׳רים של  גלריית Piwigo שלך >> צא לסיורים זמינים';
+$lang['Visit your Piwigo!'] = '!שךך Piwigo-בקר ב';
 $lang['takeatour_configpage'] = 'צא לסיור >> רשימת סיורים';
 $lang['TAT_descrp'] = 'ברוך הבא! הנה רשימה של כל הסיורים הקיימים לעזור לך לגלות את הפיצ׳רים של גלריית Piwigo שלך.<br><br>הפעל סיור למטה, ואז עקוב אחר ההוראות. ךלץ הבא/הקודם (או השתמש בחיצי המקלדת) לנווט במהלך סיור. כאשר אתה בסיור, אם אתה הולך לדף ניהול אחר, תועבר לדף העכשווי של הסיור. אם אתה תקוע ולא מצליח לסיים את הסיור, הגעה ל<em>תוספים >> קח סיור</em> תסיים את הסיור.';
+$lang['first_contact_descrp'] = 'מומלץ למתחילים, סיור זה מציג בפני Piwigo שלב ראשון בסיסי: צור אלבום והוסף את התמונות הראשונות שלך.';
+$lang['manage_albums_descrp'] = 'היררכית האלבום שלך היא המבנה של הגלריה שלך. למד כיצד לארגן אותם ולערוך את המאפיינים שלהם.';
+$lang['plugins_descrp'] = 'תוספים הם מאפיין מפתח ב- Piwigo. הפעל תכונות שונות במספר קליקים ועשה Piwigo מתאים לצרכים שלך.';
+$lang['2_9_0_descrp'] = 'גלה כמה תכונות חדשות Piwigo 2.9. קרא פרטים על <a href="%s"> הערות השחרור </a>.';
+$lang['config_descrp'] = 'קבע את הגדרות Piwigo. גלה את מנהל הנושא ובחר את המראה של Piwigo שלך.';
+$lang['edit_photos_descrp'] = 'תמונות הם היסוד הבסיסי Piwigo שלך. ראה כיצד ניתן לשנות את המאפיינים שלהם, תמונה אחת בכל פעם או באצווה.';
+$lang['edit_photos_name'] = 'לערוך תמונות
+';
+$lang['manage_albums_name'] = 'ניהול אלבומים';
+$lang['privacy_descrp'] = 'סיור זה מציג את כל הדרכים להגנה על התמונה שלך מגישה פומבית עם הרשאות לכל אלבום ותמונה, ומהעתקה באמצעות הגנות תמונה.';
+$lang['plugins_name'] = 'תוספים';
+$lang['config_name'] = 'תצורה';
+$lang['2_9_0_name'] = '2.9 סיור';
+$lang['2.8 Tour'] = '2.8 סיור';
+$lang['2_8_0_descrp'] = 'גלה כמה תכונות חדשות עם Piwigo 2.8. קרא פרטים על <a href="%s"> הערות השחרור </a>.';
