702ec10b92e6b615bfb2e69790c61c9b61c61d24
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-29 08:04:06 +0200

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_descrp'] = 'Découvrez quelques nouvelles fonctionnalités de Piwigo 2.9. Lisez les détails dans <a href="%s">les notes de version</a>.';
+$lang['Visit your Piwigo!'] = 'Visiter votre galerie Piwigo !';
+$lang['config_descrp'] = 'Configurez les paramètres de Piwigo. Découvrez le gestionnaire de thèmes et sélectionnez l\'apparence de votre galerie Piwigo.';
+$lang['edit_photos_descrp'] = 'Les photos sont les éléments de base dans votre galerie Piwigo. Regardez comment modifier leurs propriétés, une photo à la fois ou par lots.';
