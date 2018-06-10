076d99786701dd15b02c024d51759f8ebee35a1f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-06-10 08:44:09 +0200

    Update vi_VN, thanks to dummy

---
+++ language/vi_VN/tour_2_9_0.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp7'] = 'Bộ quản lý khóa hiện đã có chế độ chọn lựa riêng, để áp dụng các tính năng theo bộ.<br><br>Nhấp vào liên kết để chuyển sang chế độ chọn lựa';
+$lang['2_9_0_stp2'] = 'Phiên bản 2.0 đã đi kèm bộ phát sinh mật khẩu trực tiếp trong mẫu tạo người dùng.<br><br>Nhấp vào liên kết này và xem điều gì sẽ xảy ra!';
+$lang['2_9_0_stp4'] = 'Liên kết tải ảnh trên trang chỉnh sửa ảnh, dành cho Quản trị viên';
+$lang['2_9_0_stp5'] = 'Tìm ảnh trùng lặp dựa trên mã tổng kiểm';
+$lang['2_9_0_stp6'] = 'Mặc định, lịch sử hiện đang mở dựa trên thống kê tháng hiện tại.';
+$lang['2_9_0_title1'] = 'Chào mừng đến với Piwigo 2.0';
+$lang['2_9_0_title2'] = 'Phát sinh mật khẩu';
+$lang['2_9_0_title3'] = 'Tùy chọn khi xóa bộ ảnh';
+$lang['2_9_0_title4'] = 'Tải ảnh';
+$lang['2_9_0_title5'] = 'Trùng lặp mã tổng kiểm';
+$lang['2_9_0_title6'] = 'Lịch sử tháng hiện tại';
+$lang['2_9_0_title7'] = 'Chế độ lựa chọn khóa';
