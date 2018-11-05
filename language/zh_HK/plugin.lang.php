0fd889361dfff3feec7dbfbc373f75ba14823478
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-11-05 21:00:21 +0100

    Update zh_HK, thanks to erio

---
+++ language/zh_HK/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2.8 Tour'] = '2.8導覽';
+$lang['2_7_0_descrp'] = '發現Piwigo 2.7新功能';
+$lang['2_8_0_descrp'] = '發現Piwigo 2.8新功能。詳情參閱<a href="%s">版本說明1</a>。';
+$lang['2_9_0_descrp'] = '發現Piwigo 2.9新功能。詳情參閱<a href="%s">版本說明1</a>。';
+$lang['2_9_0_name'] = '2.9導覽';
+$lang['End tour'] = '結束導覽';
+$lang['Next '] = '下一步';
+$lang['Prev'] = '上一步';
+$lang['Start the Tour'] = '開始導覽';
+$lang['config_name'] = '設定';
+$lang['2.7 Tour'] = '2.7導覽';
