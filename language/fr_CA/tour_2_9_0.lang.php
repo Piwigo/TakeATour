4acd6247e6895e1d28e4d9978bdd895e6abc9d8b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-11-10 08:44:44 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/tour_2_9_0.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp7'] = 'Le gestionnaire de mots-clés propose maintenant un mode de sélection, à appliquer sur les actions du lot.<br/><br/>Cliquez sur le lien pour changer le mode de sélection.';
+$lang['2_9_0_title7'] = 'Mode de sélection de mots-clés';
+$lang['2_9_0_stp4'] = 'Lien de téléchargement sur la page d\'édition de photo, pour les administrateurs.';
+$lang['2_9_0_stp2'] = 'La version 2.9 inclut désormais un générateur de mot de passe dans le formulaire de création d\'un utilisateur.<br/><br/>Cliquez sur ce lien et regardez ce qui se passe !';
+$lang['2_9_0_stp6'] = 'Par défaut, l\'historique s\'ouvrira maintenant sur les statistiques du mois courant.';
