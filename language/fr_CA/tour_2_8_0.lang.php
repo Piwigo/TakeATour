f4f87f0e8a879e6ce8118bb0fb7c2653a8b28192
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-11-25 09:07:30 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/tour_2_8_0.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = 'Les détails de l\'utilisateur ne s\'ouvrent plus à l\'intérieur de la liste des utilisateurs, mais dans une "popin", une petite fenêtre dédiée à un seul utilisateur.<br><br>Piwigo 2.8 corrige aussi quelques bugs comme la mise à jour des informations dans la liste lorsque vous modifiez un utilisateur ou la vérification de l\'adresse courriel.';
+$lang['2_8_0_stp2'] = 'Quand vous notifiez un album par courriel, Piwigo 2.8 vous permet de sélectionner une liste d\'utilisateurs et pas uniquement un groupe.';
+$lang['2_8_0_stp3'] = 'Pour dire les choses simplement, les clés d\'identification signifient que vos utilisateurs n\'auront plus besoin de se souvenir de leur mot de passe. Ils accèderont au contenu privé directement et en sécurité.';
+$lang['2_8_0_title3'] = 'Clés d\'identification';
