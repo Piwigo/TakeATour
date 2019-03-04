6aea600c92d6a73fcd9dc12c53a8d74e1565dc8b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-03-04 06:34:06 +0100

    Update en_GB, thanks to Pat2309

---
+++ language/en_GB/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['manage_albums_descrp'] = 'Your album hierarchy is the structure of your gallery. Learn how to organize them and edit their properties.';
+$lang['manage_albums_name'] = 'Manage Albums';
+$lang['plugins_descrp'] = 'Plugins are a key feature in Piwigo. Activate various features in a few clicks and make Piwigo fit your needs.';
+$lang['plugins_name'] = 'Plugins';
+$lang['privacy_descrp'] = 'This tour presents all the ways to protect your photo from public access with permissions per album and per photo, and from copying using image protections.';
+$lang['takeatour_configpage'] = 'Take A Tour » List of Tours';
+$lang['2.7 Tour'] = '2.7 Tour';
+$lang['2.8 Tour'] = '2.8 Tour';
+$lang['2_7_0_descrp'] = 'Discover What\'s New in Piwigo 2.7';
+$lang['2_8_0_descrp'] = 'Discover some Piwigo 2.8 new features. Read details on <a href="%s">the release notes</a>.';
+$lang['2_9_0_descrp'] = 'Discover some Piwigo 2.9 new features. Read details on <a href="%s">the release notes</a>.';
+$lang['2_9_0_name'] = '2.9 Tour';
+$lang['End tour'] = 'End tour';
+$lang['First Contact'] = 'First Contact';
+$lang['Next '] = 'Next';
+$lang['Prev'] = 'Previous';
+$lang['Privacy'] = 'Privacy';
+$lang['Start the Tour'] = 'Start the Tour';
+$lang['TAT_descrp'] = 'Welcome! Here is a list of all the available tours to help you discover the features of your Piwigo gallery.<br><br>Launch a tour below, then follow the instructions. Click Next/Prev (or use the arrows of your keyboard) to navigate during a tour. While you are in a tour, if you go to another page of the administration, you will be redirected to the current page of the Tour. If you are stuck and can\'t end the tour, going to <em>Plugins » Take A Tour</em> will end the tour.';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Take a tour and discover the features of your Piwigo gallery » Go to the available tours';
+$lang['Visit your Piwigo!'] = 'Visit your Piwigo!';
+$lang['config_descrp'] = 'Configure your Piwigo settings. Discover the theme manager and select the appearance of your Piwigo.';
+$lang['config_name'] = 'Configuration';
+$lang['edit_photos_descrp'] = 'Photos are the basic element in your Piwigo. See how you can change their properties, one photo at a time or in a batch.';
+$lang['edit_photos_name'] = 'Edit Photos';
+$lang['first_contact_descrp'] = 'Recommended for beginners, this tour introduces you to Piwigo with a basic first step : create an album and add your first photos.';
