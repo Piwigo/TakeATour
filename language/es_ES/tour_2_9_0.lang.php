71e537cf70d314e953ca46aa6e964080b8b074a3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-05-08 18:14:42 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/tour_2_9_0.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp1'] = 'Vamos con el cambio más importante de la versión 2.9: ¡El diseño! Cabecera, pie y barra de menús en color oscuro. Menos bordes.<br><br>Página de administración totalmente nueva, llamada "Panel de control", con iconos grandes para tener acceso rápido a las páginas principales.';
+$lang['2_9_0_stp3'] = 'Al eliminar un álbum que contenga fotos, Piwigo le preguntará qué desea hacer con las fotos asociadas al álbum<br><br>Pulse en el enlace de Eliminar para verlas (cancelará la operación).';
+$lang['2_9_0_title3'] = 'Opciones en la eliminación de álbumes';
+$lang['2_9_0_stp2'] = 'La versión 2.9 incorpora un generador de contraseñas directamente en el formulario de creación de usuarios.<br><br>¡Pulse en este enlace de acción y mire lo que ocurre!';
+$lang['2_9_0_stp5'] = 'Busque fotos duplicadas basándose en el checksum o suma de verificación (coincidencia exacta en el contenido del fichero)';
+$lang['2_9_0_stp7'] = 'Ahora el gestor de etiquetas tiene un modo de selección distinto, para aplicar acciones por lotes.<br><br>Pulse en el enlace para cambiar al modo de selección.';
+$lang['2_9_0_stp6'] = 'Por defecto, el historial se abrirá en las estadísticas del mes actual.';
+$lang['2_9_0_title1'] = 'Bienvenido a Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Generador de contraseñas';
+$lang['2_9_0_title4'] = 'Descargar foto';
+$lang['2_9_0_title5'] = 'Duplicados por checksum (suma de comprobación)';
+$lang['2_9_0_title6'] = 'Historial del mes actual';
+$lang['2_9_0_title7'] = 'Modo de selección de etiquetas';
+$lang['2_9_0_stp4'] = 'Enlace de descarga en la página de edición de fotos, para administradores.';
