a35fa5aec2f0fc05818f0da31b6995778b1e81fa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-07-14 08:45:13 +0200

    Update sk_SK, thanks to dodo

---
+++ language/sk_SK/tour_2_9_0.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp1'] = 'Tu ideme s najväčšou zmenou na 2.9: design! Tmavá hlavička, päta a menu. Menej rámikov.<br><br>Zbrusu nová administračná stránka s názvom Dashboard s veľkými ikonami pre rýchly prístup na hlavné stránky.';
+$lang['2_9_0_stp2'] = 'Verzia 2.9 teraz vloží generátor hesiel priamo do formulára na vytvorenie používateľa.<br><br>Kliknite na tento odkaz akcie a zistite, čo sa stane!';
+$lang['2_9_0_stp3'] = 'Pri odstraňovaní albumu s fotografiami sa vás Piwigo teraz spýta, čo chcete robiť s fotografiami súvisiacimi s albumom.<br><br>Kliknutím na odkaz odstrániť ich zobrazíte (zrušíte).';
+$lang['2_9_0_stp4'] = 'Odkaz na stránku na úpravu fotografií pre správcov.';
+$lang['2_9_0_stp5'] = 'Nájsť duplicitné fotografie založené na kontrolnom súčte (presná zhoda v obsahu súboru)';
+$lang['2_9_0_stp6'] = 'História sa v predvolenom nastavení otvorí v štatistikách aktuálneho mesiaca.';
+$lang['2_9_0_stp7'] = 'Manažér značiek má teraz odlišný režim výberu, na použitie akcií v dávke.<br><br>Kliknutím na odkaz sa prepnete do režimu výberu.';
+$lang['2_9_0_title1'] = 'Vitajte v Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Generátor hesiel';
+$lang['2_9_0_title3'] = 'Možnosti vymazania albumu';
+$lang['2_9_0_title4'] = 'Stiahnuť fotografiu';
+$lang['2_9_0_title5'] = 'Duplikáty na kontrolnom súčte';
+$lang['2_9_0_title6'] = 'Aktuálna mesačná história';
+$lang['2_9_0_title7'] = 'Režim výberu značky';
