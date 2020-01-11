e3d209dac3634323338fc56a6c7d2813cb7e5079
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-01-11 07:52:33 +0100

    Update eo_EO, thanks to axel584

---
+++ language/eo_EO/tour_2_9_0.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_title7'] = 'Reĝimo de etikeda elekto';
+$lang['2_9_0_title3'] = 'Opcioj por albumo forigado';
+$lang['2_9_0_stp5'] = 'Trovi duobligajn fotojn per kontrolsumo (sama enhavo)';
+$lang['2_9_0_title6'] = 'Nuna monata historio';
+$lang['2_9_0_title5'] = 'Duobloj de kontrolsumo';
+$lang['2_9_0_stp6'] = 'Defaŭlte, historio nun malfermiĝas en nuna monata statistiko';
+$lang['2_9_0_title2'] = 'Generilo de pasvorto';
+$lang['2_9_0_title1'] = 'Bonvenon en Piwigo 2.9';
+$lang['2_9_0_title4'] = 'Elŝuti foton';
