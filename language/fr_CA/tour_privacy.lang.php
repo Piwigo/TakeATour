e2cd7b8eb30b5e3d6c6d8640606b342b7f1363b0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-12-03 19:00:24 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/tour_privacy.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['privacy_stp9'] = '... et sélectionnez l\'action "Qui peut voir les photos ?". Vous pouvez ainsi définir le niveau de confidentialité des photos sélectionnées.<br><br>Mais vous pouvez aussi le faire pour une photo spécifique, sur sa page d\'édition. Vous pouvez y accéder depuis la partie publique, ou à partir d\'ici.<br><br>Maintenant, voyons l\'autre système de permissions pour les albums fondés sur les groupes et les utilisateurs.';
