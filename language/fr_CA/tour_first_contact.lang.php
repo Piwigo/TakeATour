859c740b3afcdd9c373e1dc605deb7dff57f8299
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-12-25 20:23:14 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/tour_first_contact.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_title1'] = 'Bienvenue dans votre galerie Piwigo';
+$lang['first_contact_stp13'] = 'Vous pouvez éditer les descriptions, mots-clés et autres propriétés depuis la gestion par lot en mode unitaire. Mais pour le moment, restons en mode global.';
