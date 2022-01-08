24e1b44474a364c9327e9b370a7fba0e6585cd5b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-08 07:08:22 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/tour_privacy.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['privacy_stp2'] = 'Tukaj lahko dostopate do integrirane pomoči. Kliknite Naprej, da nadaljujete neposredno na stran s pomočjo o upravljanju dovoljenj';
+$lang['privacy_stp19'] = 'Tukaj lahko hitro uredite posameznega uporabnika, tako da miškin kazalec premaknete nanj in nato kliknete povezavo Uredi.<br><br>Izberete lahko več uporabnikov in jih uredite hkrati, tako da nato izberete dejanje, ki ga želite uporabiti.';
+$lang['privacy_stp18'] = 'Če želite upravljati povezave med uporabniki in skupinami, pojdite na stran <em>Uporabniki » Upravljanje</em>';
+$lang['privacy_stp16'] = 'Tako kot v računalniku lahko izberete več albumov s tipkama Shift in Control, nato pa kliknete puščico, da jih preklopite.<br><br>Zdaj naj vam predstavim skupine.';
+$lang['privacy_stp15'] = 'Tik pred razlago skupin, nasvet za profesionalce: tukaj je povezava do strani za nastavitev več albumov za zasebne/javne hkrati. Kliknite na to';
+$lang['privacy_stp14b'] = 'Pomembno dejstvo: na javni strani veljajo dovoljenja za spletne skrbnike in skrbnike kot za vse druge uporabnike. V upravi lahko dostopajo do katerega koli albuma ali fotografije.';
+$lang['privacy_stp14'] = 'Nato se lahko uporabnikom in skupinam uporabnikov odobri dostop do albuma.';
+$lang['privacy_stp17'] = 'Skupina v Piwigu je le nabor uporabnikov: tako skupine olajšajo upravljanje dovoljenj za albume in upravljanje lastnosti uporabnikov. Tukaj je stran, kjer lahko upravljate skupine, torej jih preimenujete, združite, podvojite, izbrišete. Eno ali več skupin lahko nastavite tudi kot "privzeto skupino", kar pomeni, da bodo na novo registrirani uporabniki povezani s temi privzetimi skupinami.';
+$lang['privacy_stp21'] = 'Rešitve so:<ul><li>dajte vodni žig vsaj na fotografije srednje in visoke ločljivosti.</li><li>in onemogočite velikosti XL in XXL.</li><li>in onemogočite visoko ločljivost (prenos in prikaz izvirne fotografije) za zadevne uporabnike.';
+$lang['privacy_stp24'] = 'Ta turneja je končana.<p style="text-align:center">Uživajte v svojem Piwigu!</p>Če vam je Piwigo všeč in nas želite podpreti, lahko prispevate s pomočjo pri prevodih, donacijah itd. <a href="https://piwigo.org/basics/contribute" target="_blank">Kliknite tukaj, da nas podprete</a>. Hvala!';
+$lang['privacy_stp20'] = 'Zdaj veste, kako ohraniti svoje fotografije zasebne, vendar se morda sprašujete, kako zaščititi svoje javne fotografije. Morda boste najprej pomislili, da bi obiskovalcu blokirali prenos fotografije: ne morete, ker je bil splet tako ustvarjen (brskalnik obiskovalca prenese vse prikazane vire in še več). Desni klik je mogoče onemogočiti, na fotografijo lahko postavite prazen sloj itd., vendar prenosa ne bo onemogočil. Celotne spletne strani lahko shranite s katerim koli spletnim brskalnikom.';
+$lang['privacy_stp7'] = '... ali pa to storite pozneje, tukaj v Upravitelju paketov v globalnem načinu.<br><br>To je stran <em>Fotografije » Nedavne fotografije</em>, zato je nastavljen filter "Zadnji uvoz".';
+$lang['privacy_stp6'] = 'Ko naložite fotografije, lahko spremenite raven zasebnosti naloženih fotografij kar tam ...';
+$lang['privacy_stp5'] = 'Spodaj je razloženo, kako lahko upravljate Skupine.<br>Ta dovoljenja so samo za dostop do fotografij in albumov, pri brskanju na javnem delu ali za zunanje aplikacije. Kasneje bomo videli druge zaščite, zdaj pa vadimo!';
+$lang['privacy_stp4'] = 'Tako imamo dva sistema za upravljanje dovoljenj za dostop do fotografij. So neodvisni, tako da lahko ustvarite skupino z imenom Družina, vendar ta skupina nima nobene zveze s stopnjo zasebnosti Družina. <br><br>Raven zasebnosti se uporablja za fotografijo, skupinska/uporabniška dovoljenja pa za album. Uporabite lahko oba ali samo en sistem, ki je prilagodljiv.<br><br>Naslednji zavihek je o upravljanju skupin.';
+$lang['privacy_stp3'] = 'Vzemite si trenutek in preberite spodnje informacije.';
+$lang['privacy_stp8'] = 'Izberite eno ali več fotografij ...';
+$lang['privacy_stp9'] = '... in izberite dejanje "Kdo lahko vidi fotografije?". Zdaj lahko spremenite raven zasebnosti izbranih fotografij.<br><br>Lahko pa to storite tudi za določeno fotografijo na strani za urejanje. Do nje lahko dostopate iz javnega dela ali od tukaj.<br><br>Zdaj vam bom pokazal drugi sistem dovoljenj za album glede na skupine in uporabnike.';
+$lang['privacy_title1'] = 'Dobrodošli na ogledu zasebnosti';
+$lang['privacy_title10'] = 'Dovoljenja za album';
+$lang['privacy_title11'] = 'Dovoljenja za album';
+$lang['privacy_title12'] = 'Dovoljenja za album';
+$lang['privacy_title22'] = 'Lokalna konfiguracija: Izvirna zaščita';
+$lang['privacy_title15'] = 'Nasvet';
+$lang['privacy_title2'] = 'Pomoč v vašem Piwigu';
+$lang['privacy_title19'] = 'Uporabniki';
+$lang['privacy_title17'] = 'Skupine';
+$lang['privacy_title18'] = 'Skupine';
+$lang['privacy_title3'] = 'Dovoljenja';
+$lang['privacy_title4'] = 'Dovoljenja';
+$lang['privacy_title24'] = 'Bilo je super';
+$lang['privacy_title20'] = 'Javne fotografije';
+$lang['privacy_title21'] = 'Javne fotografije';
+$lang['privacy_title5'] = 'Dovoljenja > Skupine';
+$lang['privacy_title6'] = 'Raven zasebnosti';
+$lang['privacy_title7'] = 'Raven zasebnosti';
+$lang['privacy_title8'] = 'Raven zasebnosti';
+$lang['privacy_title9'] = 'Raven zasebnosti';
+$lang['privacy_stp22'] = '<em>Za napredne uporabnike</em><br><br>V Piwigu lahko zaščitite izvirne fotografije z lokalno konfiguracijo. Uporabite spremenljivko $conf[\\'original_url_protection\\']: privzeto je prazno, vrednost nastavite na "images", da zaščitite samo fotografije, ali "all", da zaščitite tudi vse vrste medijev, ki lahko porabijo vire ali pa preprosto ne deluje na vašem strežniku.<br><br>Ta možnost deluje za javno in zasebno vsebino. Ta možnost trenutno zahteva, da zavrnete dostop do map /upload in /galleries z uporabo datoteke .htaccess (običajno besedilne datoteke z »Zavrni vse« za vsebino) ali konfiguracije strežnika.<br><br>Prosim upoštevajte, da so imena datotek fotografij, naloženih z drugo metodo kot FTP, <b>naključno razporejena</b>, zato jih je nemogoče uganiti: to ime datoteke in tako pot do izvirne fotografije je mogoče poznati le, če ima obiskovalec dostop do spremenjene velikosti različico te fotografije, kot je sličica. $conf[\\'original_url_protection\\'] in zavrnitev dostopa do map /upload in /galleries naj bi se izognila temu primeru.';
+$lang['privacy_stp13'] = 'Zdaj kliknite na zasebno.';
+$lang['privacy_stp1'] = 'Zdravo! Jaz bom vaš vodič pri odkrivanju, kako zaščititi svoje fotografije v Piwigu. Sledite mojim navodilom in kliknite Naprej (ali uporabite puščice na tipkovnici) za navigacijo. Če greste na drugo stran administracije, boste preusmerjeni na trenutno stran predstavitve. Če ste obtičali in ne morete končati predstavitve, boste na <em>Vtičniki » Začni predstavitev</em> končali predstavitev.<br>Začnimo!';
+$lang['privacy_stp10'] = 'Smo v upravljanju seznama albumov, ki je na voljo v meniju z <em>Albumi » Upravljanje</em><br><br>Uredite album tako, da ga premaknete in nato kliknite Uredi.';
+$lang['privacy_stp11'] = 'Zdaj kliknite zavihek Dovoljenja';
+$lang['privacy_stp12'] = 'Na tej strani lahko izberete, ali bo album na voljo vsem ali bo omejen na nekatere uporabnike';
