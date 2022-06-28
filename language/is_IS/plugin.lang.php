4eb7de85dc3e710b20e6897e71b578dd8558a346
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-28 07:24:37 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_descrp'] = 'Mælt með fyrir byrjendur, þessi kynning kynnir þér fyrir Piwigo með fyrstu grunnskrefum : stofnaðu möppu og settu inn þínar fyrstu myndir.';
+$lang['manage_albums_descrp'] = 'Albúmstigveldið þitt er uppbygging gallerísins þíns. Lærðu hvernig á að skipuleggja þau og breyta eiginleikum þeirra.';
+$lang['manage_albums_name'] = 'Stjórna möppum';
+$lang['plugins_descrp'] = 'Viðbætur eru lykileiginleiki í Piwigo. Virkjaðu ýmsa eiginleika með nokkrum smellum og láttu Piwigo mæta þínum þörfum.';
+$lang['plugins_name'] = 'Viðbætur';
+$lang['privacy_descrp'] = 'Þessi kynning kynnir allar leiðir til að vernda myndina þína fyrir aðgangi almennings með heimildum fyrir hvert albúm og hverja mynd, og frá afritun með myndvörnum.';
+$lang['takeatour_configpage'] = 'Fá kynningu » Listi yfir kynningar';
+$lang['TAT_descrp'] = 'Velkominn! Hér er listi yfir allar tiltækar kynningar til að hjálpa þér að uppgötva mögueikana í þínu Piwigo myndasafni.<br><br>Setja af stað kynningu að neðan, fylgdu síðan leiðbeiningunum. Smellið á Næsta/Fyrri (eða notaðu örvarlykla á lyklaborðinu þínu) til að skoða á meðan á kynningu stendur. Á meðan þú ert á kynningu, ef þú ferð á aðra stjórnunarsíðu, verður þér vísað á núverandi síðu kynningarinnar. Ef þú ert fastur og getur ekki endað kynninguna lýkur kynningunni með því að fara á <em>Viðbætur » Fá kynningu</em>.';
+$lang['Take a tour and discover the features of your Piwigo gallery » Go to the available tours'] = 'Fáðu kynningu og uppgötvaðu möguleikana í þínu Piwigo myndasafni » Farðu í þær kynningar sem eru í boði';
+$lang['Visit your Piwigo!'] = 'Heimsækja þitt Piwigo!';
+$lang['config_descrp'] = 'Stilla þínar Piwigo stillingar. Uppgötvaðu þemastjórann og veldu útlitið á þínu Piwigo.';
+$lang['config_name'] = 'Stillingar';
+$lang['edit_photos_descrp'] = 'Myndir eru grunnþátturinn í þínu Piwigo. Sjáðu hvernig þú getur breytt eiginleikum þeirra, einni mynd í einu eða í lotu.';
+$lang['edit_photos_name'] = 'Breyta myndum';
+$lang['2.7 Tour'] = '2.7 Kynning';
+$lang['2.8 Tour'] = '2.7 Kynning';
+$lang['2_7_0_descrp'] = 'Uppgötvaðu hvað er nýtt í Piwigo 2.7';
+$lang['2_8_0_descrp'] = 'Uppgötvaðu nokkra nýja möguleika í Piwigo 2.8. Lestu nánari upplýsingar í <a href="%s">útgáfu upplýsingar</a>.';
+$lang['2_9_0_descrp'] = 'Uppgötvaðu nokkra nýja möguleika í Piwigo 2.9. Lestu nánari upplýsingar í <a href="%s">útgáfu upplýsingar</a>.';
+$lang['2_9_0_name'] = '2.9 Kynning';
+$lang['End tour'] = 'Hætta við kynningu';
+$lang['First Contact'] = 'Fyrstu tengsl';
+$lang['Next '] = 'Næsta';
+$lang['Prev'] = 'Fyrri';
+$lang['Privacy'] = 'Friðhelgi';
+$lang['Start the Tour'] = 'Hefja kynninguna';
