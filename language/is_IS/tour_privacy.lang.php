628735687aa661ead699eba5de4bab5a9e8d70c2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-08-20 08:48:38 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/tour_privacy.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['privacy_stp22'] = '<em>Fyrir lengra komna notendur</em><br><br>Í Piwigo geturðu verndað upprunalegu myndirnar með því að nota staðbundna uppsetningu. Notaðu breytuna $conf[\'original_url_protection\']: sjálfgefið tómt, stillirðu gildið á "myndir" til að vernda aðeins myndirnar eða "allar" til að vernda líka allar tegundir miðla, sem getur verið auðlinda krefjandi eða gæti einfaldlega ekki virkað á netþjóninum þínum.<br><br>Þessi valkostur virkar fyrir opinbert og einkaefni. Þessi valkostur krefst þess að þú hafnar aðgangi að möppunum /upload og /galleries, með því að nota .htaccess skrá (venjulega textaskrá með "Deny from all" fyrir efni) eða stillingar netþjónsins.<br><br>Vinsamlegast Athugaðu að skráarnöfn mynda sem hlaðið er upp með annarri aðferð en FTP eru <b>slembiraðað</b>, svo ómögulegt er að giska á þau: það skráarnafn og svo slóð upprunalegu myndarinnar er aðeins hægt að vita ef gesturinn hefur aðgang að breyttri stærðarútgáfu af þeirri mynd, eins og smámyndina. $conf[\'original_url_protection\'] og að neita aðgangi að möppunum /upload og /galleríum er ætlað að forðast það tilvik.';
+$lang['privacy_stp24'] = 'Þessari leiðsögn er lokið. <p style="text-align:center">Njóttu Piwigo!</p>Ef þér líkar við Piwigo og langar að styðja okkur, þá getur þú lagt okkur lið með því að hjálpa við þýðingar, fjárstuðnng, o.s.frv. <a href="http://piwigo.org/basics/contribute" target="_blank">Smelltu hér til að styðja okkur</a>. Takk!';
+$lang['privacy_stp20'] = 'Núna veistu hvernig á að halda myndunum þínum sem einkamál, en þú gætir velt því fyrir þér hvernig eigi að vernda opinberu myndirnar þínar. Þú gætir fyrst hugsað að loka fyrir gestinn til að hlaða niður myndinni: þú getur það ekki, vegna þess að vefurinn hefur verið útbúinn þannig (vafri gestsins hleður niður öllum auðlindum sem sýnd eru og fleira). Hægt er að slökkva á hægrismellinum, setja autt lag ofan á myndina o.s.frv. en það mun ekki slökkva á niðurhalinu. Hægt er að vista allar vefsíður með hvaða vafra sem er.';
+$lang['privacy_stp4'] = 'Þannig að við höfum tvö kerfi til að stjórna aðgangsheimildum að myndunum. Þeir eru óháðir, svo þú getur búið til hóp sem heitir Fjölskylda, en sá hópur hefur ekkert með friðhelgisstigið Fjölskylda að gera. <br><br>Friðhelgisstigum er beitt fyrir hverja mynd og hóp-/notendaheimilda er beitt fyrir hvert albúm. Þú getur notað bæði eða aðeins eitt kerfi, það er sveigjanlegt.<br><br>Næsti flipi er um hópstjórnun.';
+$lang['privacy_stp9'] = '... og veldu aðgerðina "Hver getur séð myndirnar?". Nú geturðu breytt friðhelgisstigi valinna mynda.<br><br>En þú getur líka gert það fyrir tiltekna mynd, á breytingasíðu hennar. Þú getur fengið aðgang að því frá opinbera hlutanum, eða héðan.<br><br>Nú mun ég sýna þér annað heimildakerfi fyrir hvert albúm byggt á hópum og notendum.';
+$lang['privacy_stp18'] = 'Til að hafa umsjón með tengingum milli notenda við hópa, farðu í <em>Notendur » Umsýsla</em> síðuna';
+$lang['privacy_stp19'] = 'Hér getur þú fljótt breytt einum notanda með því að fara með músina yfir hann og smella svo á Breyta hlekkinn.<br><br>Þú getur valið marga notendur og breytt þeim í einu með því að velja síðan aðgerð til að nota.';
+$lang['privacy_stp2'] = 'Hér geturðu fengið aðgang að samþættu hjálpinni. Smelltu á Næst til að halda beint áfram á hjálparsíðuna um stjórnun heimilda';
+$lang['privacy_stp21'] = 'Lausnirnar eru:<ul><li>settu vatnsmerki, að minnsta kosti á miðlungs og háskerpu myndir.</li><li>og slökktu á XL og XXL stærðum.</li><li>og slökktu á háskerpu (hlaða niður og birta upprunalegu myndina) fyrir viðkomandi notendur.';
+$lang['privacy_stp5'] = 'Hér að neðan er útskýrt hvernig þú getur stjórnað hópunum.<br>Þessar heimildir eru aðeins fyrir aðgang að myndunum og albúmunum, þegar vafrað er í opinbera hlutanum eða fyrir ytri forritin. Við munum sjá síðar aðrar varnir, en nú skulum við æfa okkur!';
+$lang['privacy_stp7'] = '... eða þú getur gert það seinna, hér í lotustjóranum í almennum ham.<br><br>Þetta er síðan <em>Myndir » Nýlegar myndir</em>, þannig að sían "Síðasti innflutningur" er virk.';
+$lang['privacy_title22'] = 'Staðbundin stilling: Upprunaleg vernd';
+$lang['privacy_title24'] = 'Þetta hefur verið frábær tími';
+$lang['privacy_title3'] = 'Heimildir';
+$lang['privacy_title4'] = 'Heimildir';
+$lang['privacy_title5'] = 'Heimildir > Hópar';
+$lang['privacy_title6'] = 'Friðhelgisstig';
+$lang['privacy_title7'] = 'Friðhelgisstig';
+$lang['privacy_title8'] = 'Friðhelgisstig';
+$lang['privacy_title9'] = 'Friðhelgisstig';
+$lang['privacy_stp3'] = 'Gefðu þér smá stund til að lesa upplýsingarnar hér að neðan.';
+$lang['privacy_stp6'] = 'Þegar þú hleður upp myndunum geturðu breytt friðhelgisstigi myndanna sem hlaðið var upp þar...';
+$lang['privacy_stp8'] = 'Veldu eina eða fleiri myndir...';
+$lang['privacy_title17'] = 'Hópar';
+$lang['privacy_title18'] = 'Hópar';
+$lang['privacy_title19'] = 'Notendur';
+$lang['privacy_title2'] = 'Hjálp í þínu Piwigo';
+$lang['privacy_title20'] = 'Opinberar myndir';
+$lang['privacy_title21'] = 'Opinberar myndir';
+$lang['privacy_title1'] = 'Velkominn í einka leiðsögnina';
+$lang['privacy_title10'] = 'Albúm heimildir';
+$lang['privacy_title11'] = 'Albúm heimildir';
+$lang['privacy_title12'] = 'Albúm heimildir';
+$lang['privacy_title15'] = 'Hugmynd';
+$lang['privacy_stp17'] = 'Hópur í Piwigo er bara hópur notenda: þannig að hópar auðvelda stjórnun albúmheimilda og stjórnun eiginleika notenda. Hérna er síðan þar sem þú getur stjórnað hópum, t.d. endurnefna, sameina, afrita, eyða þeim. Þú getur líka stillt einn eða fleiri hópa sem „sjálfgefinn hóp“ sem þýðir að nýskráðir notendur verða tengdir þeim sjálfgefnu hópum.';
+$lang['privacy_stp1'] = 'Halló! Ég mun vera leiðbeinandi þinn í að uppgötva hvernig á að vernda myndirnar þínar í Piwigo. Vinsamlegast fylgdu leiðbeiningunum mínum og smelltu á Næst (eða notaðu örvarnar á lyklaborðinu þínu) til að fletta. Ef þú ferð á aðra stjórnunarsíðu verður þér vísað á núverandi síðu leiðsagnarinnar. Ef þú ert fastur og getur ekki endað leiðsögnina, farður þá í <em>Viðbætur » Fá leiðsögn</em> og það mun enda ferðina.<br>Við skulum byrja!';
+$lang['privacy_stp10'] = 'Við erum í stjórnun albúmlista sem er tiltæk í valmyndinni með <em>Albúm » Umsýsla</em><br><br>Breyttu albúmi með því færa músarbendilinn yfir það og smella síðan á Breyta.';
+$lang['privacy_stp11'] = 'Smelltu núna á flipann Heimildir';
+$lang['privacy_stp12'] = 'Á þeirri síðu geturðu valið hvort albúmið verði tiltækt fyrir hvern sem er eða verði takmarkað við suma notendur';
+$lang['privacy_stp13'] = 'Smelltu núna á einkamál.';
+$lang['privacy_stp14'] = 'Þá er hægt að veita notendum og notendahópum aðgang að albúminu.';
+$lang['privacy_stp14b'] = 'Mikilvæg staðreynd: á opinbert, gilda heimildir fyrir vefstjóra og stjórnendur eins og hvern annan notanda. Á stjórnborðinu geta þeir nálgast hvaða albúm eða mynd sem er.';
+$lang['privacy_stp15'] = 'Rétt áður en við útskýrum hópana, ábending fyrir stjórnendur: hér er hlekkur á síðu til að stilla sem einkamál/opinbert nokkur albúm í einu. Smelltu á það';
+$lang['privacy_stp16'] = 'Eins og á tölvunni þinni geturðu valið nokkur albúm með tökkunum Shift og Control, smelltu svo á örina til að skipta um þau.<br><br>Núna, skal ég kynna hópana.';
