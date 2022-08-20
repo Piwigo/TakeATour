93a5609b53bb7b396efc75086881941a50323548
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-08-20 08:48:49 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/tour_first_contact.lang.php
@@ -0,0 +1,65 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['first_contact_title39'] = 'Viðbætur';
+$lang['first_contact_title4'] = 'Við skulum bæta við mynd';
+$lang['first_contact_title43'] = 'Þetta hefur verið frábær tími';
+$lang['first_contact_title9'] = 'Lotustjórinn';
+$lang['first_contact_stp6'] = 'Þú sérð að verið er að skrá myndir. Þegar þú ert tilbúinn skaltu smella á hnappinn Hefja innflutning';
+$lang['first_contact_stp7'] = 'Hér er samantekt yfir myndirnar þínar sem þú hefur hlaðið upp. Sjáðu að Piwigo hefur þegar búið til smámyndirnar. Þú getur:<ul><li>stjórnað öllum myndum sem hlaðið er upp í lotustjóranum</li><li>breytt eiginleikum myndar með því að smella á hana</li><li>bæta við fleiri myndum</li></ul>';
+$lang['first_contact_stp8'] = 'Við skulum breyta þeim! Smelltu á hlekkinn.';
+$lang['first_contact_stp9'] = 'Þú ert núna í lotustjóranum, þar sem þú getur hópbreytt mörgum myndum.<br><br>Hér er forsían þegar stillt á "Karfa" vegna þess að við komum frá upphleðslusíðunni.';
+$lang['first_contact_title1'] = 'Velkominn í þitt Piwigo myndasafn';
+$lang['first_contact_title11'] = 'Hvernig á að breyta mynd';
+$lang['first_contact_title15'] = 'Breytingarsíðan fyrir mynd';
+$lang['first_contact_title18'] = 'Stjórna albúmum';
+$lang['first_contact_title19'] = 'Stjórna Albúmum';
+$lang['first_contact_title20'] = 'Stjórna Albúmum';
+$lang['first_contact_title21'] = 'Aðrir flipar';
+$lang['first_contact_title22'] = 'Albúm breytingarsíða';
+$lang['first_contact_title28'] = 'Stillingar';
+$lang['first_contact_title29'] = '<em>Stillingar » Möguleikar » Almennt</em>';
+$lang['first_contact_title3'] = 'Bæta við Myndasíðu';
+$lang['first_contact_title33'] = 'Gestastillingar';
+$lang['first_contact_title34'] = 'Þemu';
+$lang['first_contact_title35'] = 'Þemu';
+$lang['first_contact_title37'] = 'Þemu';
+$lang['first_contact_title38'] = 'Viðbætur';
+$lang['first_contact_stp30'] = 'Byrjum á því að breyta titlinum';
+$lang['first_contact_stp31'] = 'Síðan borðann, sem birtist efst á hverri síðu. Eins og þú sérð eru HTML kóði leyfður hér. Þú getur líka notað %gallery_title% merkið til að birta titilinn sem þú slóst inn hér að ofan.';
+$lang['first_contact_stp32'] = 'Vistaðu nú breytingarnar';
+$lang['first_contact_stp33'] = 'Aðeins lokaorð um valkostina, \'Gestastillingar\' síðan er til að stilla óskir óskráðra gesta. Í hvert skipti sem Piwigo vísar til „gestur“ þýðir það óskráða gesti.<br>Höldum áfram með stillingar: smelltu á <em>Stillingar » Þemu</em> eða ýttu bara á Næst.';
+$lang['first_contact_stp34'] = 'Að velja þema er fyrsta skrefið til að sérsníða þitt Piwigo. Uppsett þemu eru skráð hér.<br>Hægt er að virkja fleiri en eitt þema: notendur geta breytt sínu þema með því að velja eitt af þeim sem eru virkjuð hér, ef hakað er við valkostinn \'Leyfa aðlögun notanda\' á síðunni Möguleikar.';
+$lang['first_contact_stp35'] = 'Stilltu uppáhalds þemað þitt sem sjálfgefið þema.';
+$lang['first_contact_stp37'] = 'Til að setja upp ný þemu geturðu hlaðið þeim beint niður af flipanum \'Bæta við þema\'. Aðeins þemu sem eru merkt sem samhæf við þína útgáfu af Piwigo eru sýnd.<br><br>Við skulum skoða viðbætur núna! Smelltu á <em>Viðbætur » Stjórna</em>';
+$lang['first_contact_stp38'] = 'Viðbætur eru mjög auðveld leið til að sérsníða þitt Piwigo. Þær geta gert næstum allt sem þú getur ímyndað þér, allt frá litlum textaviðbótum til fullkominna eiginleika, eins og Samfélags viðbótina sem gerir notendum öðrum en stjórnendum kleift að hlaða upp án þess að fara inn í stjórnunarhlutann.';
+$lang['first_contact_stp39'] = 'Á fyrstu síðunni eru uppsettar viðbætur skráðar.<br><br>Efst eru skráðar viðbætur virkjaðar, sem eru í gangi.<br><br>Neðst eru viðbæturnar sem eru uppsettar í / möppuna viðbætur, en óvirk.<br><br>Ef viðbót er eytt þýðir að öll ummerki um viðbótina verða fjarlægð (skrár, valkostir osfrv.). Fyrir flestar viðbætur þá mun afvirkja halda gögnunum skráðum af viðbótinni.';
+$lang['first_contact_stp4'] = 'Veldu fyrst albúm; búðu til eitt ef ekkert albúm þegar til.';
+$lang['first_contact_stp40'] = 'Þetta er ég hérna! Ekki afvirkja mig núna, en þú getur séð að þú getur afvirkjað eða endurheimt virkjaða viðbót.';
+$lang['first_contact_stp41'] = 'Þú getur séð á síðasta flipanum allar viðbætur sem eru tiltækar fyrir þína útgáfu af Piwigo. Hundrað viðbætur eru þarna!';
+$lang['first_contact_stp43'] = 'Þessi leiðsögn var frekar löng, en við höfum aðeins séð lítinn hluta af því hversu öflugur Piwigo er. Allt hefur sinn endi, þessu yfirliti er lokið.<br><br>Þú getur rannsakað betur eiginleika Piwigo með því að fara í aðra skoðunarferð eða lesa skjölin okkar á piwigo.org vefsíðunni.<br><br>Ekki gleyma líka Hjálparhlekknum efst í hægra horninu á hvaða stjórnunarsíðu sem er.<p style="text-align:center">Njóttu Piwigo!</p>';
+$lang['first_contact_stp5'] = 'Smelltu síðan á hnappinn til að velja myndir til að senda, eða dragðu og slepptu skránum þínum.';
+$lang['first_contact_stp11'] = 'Héðan er hægt að velja eina eða fleiri myndir';
+$lang['first_contact_stp12'] = 'og breyta þeim síðan með aðgerð';
+$lang['first_contact_stp13'] = 'Þú getur breytt lýsingum og fleiru í lotustjóranum í einni aðgerð. En í bili skulum við vera í almennum ham';
+$lang['first_contact_stp14'] = 'Nú munum við sjá <b>breytingasíðu</b> á einni mynd. Hægt er að nálgast þá síðu héðan en einnig frá opinberu myndasíðunni þegar þú ert skráður sem stjórnandi.<br><br>Færðu bendilinn yfir smámyndina og smelltu síðan á Breyta.';
+$lang['first_contact_stp15'] = 'Á þessari síðu er hægt að breyta öllum eiginleikum myndar, til dæmis...';
+$lang['first_contact_stp16'] = '...í hvaða albúmi(um) myndin er.<br><br>Þannig að hér geturðu bætt við eða fjarlægt myndina úr albúmum. Tengillinn er sýndartengill, engar myndir verða fluttar nokkru sinni raunlægt.';
+$lang['first_contact_stp17'] = 'Hér getur þú stillt myndina sem smámynd fyrir eitt eða nokkur albúm. Myndin þarf ekki að vera tengd albúminu til að vera smámynd þess. Ef myndin er tengd albúminu er enn einfaldara að stilla hana sem smámynd fyrir albúm af opinberri síðu myndarinnar (ábending: notaðu viðbótina Stjórnunarverkfæri fyrir enn meiri einfaldleika).';
+$lang['first_contact_stp18'] = 'Við skulum sjá núna hvernig á að stjórna albúmum. Smelltu á <em>Á albúmi » Stjórna</em> eða ýttu á Næsta';
+$lang['first_contact_stp19'] = 'Hér eru skráð \'rótar\' albúm. Ef þú sérð albúm með strikuðum bakgrunni þýðir það raunlægt albúm frá FTP samstillingu. Ekki er hægt að færa eða eyða raunlægum albúmum héðan. Hin albúmin eru kölluð sýndaralbúm.';
+$lang['first_contact_stp19_b'] = 'Hér eru skráð \'rótar\' albúm.';
+$lang['first_contact_stp2'] = 'Linkurinn er hér. Smelltu á hann eða smelltu á Næst til að halda áfram';
+$lang['first_contact_stp20'] = 'Þegar músin þín sveimar yfir albúmi birtast tenglar og upplýsingar. Þegar þú dregur og sleppir albúmi - ávöl blokk-, muntu breyta staðsetningu þess og þá geturðu vistað eða hætt við handvirka röðun.';
+$lang['first_contact_stp21'] = 'Næsti flipi er síða þar sem þú getur fært hvaða sýndaralbúm sem er.<br>Fasttengla flipinn er til að útbúa ákveðna slóð fyrir albúm.<br>En við skulum breyta albúmi: smelltu á Breyta albúmi';
+$lang['first_contact_stp22'] = 'Eins og fyrir mynd, hér geturðu breytt eiginleikum albúms';
+$lang['first_contact_stp23'] = 'Að læsa albúmi þýðir að aðeins stjórnendur geta séð það albúm: mjög gagnlegt þegar verið að undirbúa albúmið fyrir útgáfu';
+$lang['first_contact_stp28'] = 'Nú munum við skoða valkostina sem eru tiltækir til að láta Piwigo vinna. Smelltu á Stillingar->Möguleikar eða smelltu bara á Næst.';
+$lang['first_contact_stp29'] = 'Hér, á fyrstu síðunni, muntu stilla nauðsynlegar og grunnstillingar á galleríinu þínu.';
+$lang['first_contact_stp3'] = 'Þessi fyrsti flipi er þar sem þú bætir við myndum beint frá vafranum þínum. Hinir fliparnir fjalla um aðrar leiðir til að hlaða upp myndum';
+$lang['first_contact_stp10'] = 'Hér er hlekkurinn til að fá beinan aðgang að körfunni þinni: þetta er tól fyrir stjórnendur til að velja myndir á auðveldan hátt þegar þeir vafra um opinbera hlutann til að breyta þeim í lotustjóranum.<br><br>Þú getur bætt myndum við körfuna frá opinberri myndasíðu þegar þú ert skráður inn sem stjórnandi, eða með því að nota aðgerðina „Bæta við körfu“ í lotustjóranum.';
+$lang['first_contact_stp1'] = 'Halló! Ég mun vera leiðbeinandi þinn við að uppgötva Piwigo. Vinsamlegast fylgdu leiðbeiningunum mínum og smelltu á Næst (eða notaðu örvarnar á lyklaborðinu þínu) til að fletta. Ef þú ferð á aðra stjórnunarsíðu verður þér vísað á núverandi síðu leiðsagnarinnar. Ef þú ert fastur og getur ekki endað leiðsögnina, með því að fara á <em>Viðbætur » Fá leiðsögn</em> þá mun það enda leiðsögnina.<br>Byrjum á því að bæta við myndum!';
