ac276822de3865dc481e4b8aff015e8aec00abfb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-08-23 21:09:31 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/tour_2_7_0.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['2_7_0_title8'] = 'Tæma körfuna þína';
+$lang['2_7_0_title9'] = 'Leitarsía';
+$lang['2_7_0_stp2b'] = 'Nýja upphleðsluformið í Piwigo 2.7 er byggt á HTML5 í stað Flash, það þýðir betri samhæfni við nútíma vefvafra auk aukins áreiðanleika á hægum tengingum. Og draga og sleppa mun gera val á myndum miklu auðveldara úr skráarvafranum þínum!';
+$lang['2_7_0_stp4'] = 'Leitarsíðan hefur verið endurgerð: möguleiki á að velja eiginleika sem orðaleit á við um, listi yfir höfunda sem finnast í gagnagrunni, sjálfvirkur útfyllingarlisti yfir merki, sjálfvirkur útfyllingarlisti yfir albúm...';
+$lang['2_7_0_stp5'] = 'Þú getur stillt tímann til viðbótar við dagsetninguna með því að nota klukkutíma og mínútu sleða';
+$lang['2_7_0_stp6'] = 'Valreiturinn til að velja albúm, merki o.s.frv. hefur breyst í nýjan sem er notendavænni. Prófaðu hann hér að neðan';
+$lang['2_7_0_stp7'] = 'Nú geturðu flokkað albúmin þín út frá stofnunardasetningu mynda eða birtingardagsetningu';
+$lang['2_7_0_stp8'] = 'Auðveldur aðgerðartengill til að tæma körfuna þína með einum smelli.';
+$lang['2_7_0_stp9'] = 'Leitarsíu hefur verið bætt við lotustjórann. Hún kemur með öflugri setningafræði til að leita að myndum eftir dagsetningum, merkjum, stærð, skráarnafni og einnig leitarvalkostum eins og nákvæmri samsvörun, AND/OR, flokkun, útilokun. Þú getur lesið ítarlegu hjálpina síðar. Sama setningafræði á við um flýtileitarreitinn í almennri hliðarvalmynd.';
+$lang['2_7_0_title1'] = 'Velkominn í Piwigo 2.7';
+$lang['2_7_0_title10'] = 'Skráarstærðar sía';
+$lang['2_7_0_title11'] = 'Sjáumst fljótlega!';
+$lang['2_7_0_title2'] = 'Fyrsti nýi eiginleikinn';
+$lang['2_7_0_title2b'] = 'Nýtt upphleðslueyðublað';
+$lang['2_7_0_title4'] = 'Leitarsíða';
+$lang['2_7_0_title5'] = 'Tími';
+$lang['2_7_0_title6'] = 'Veldu kassa';
+$lang['2_7_0_title7'] = 'Raða albúmum eftir dagsetningu';
+$lang['2_7_0_stp11'] = 'Þessari leiðsögn er lokið.<p style="text-align:center">Njóttu þíns Piwigo 2.7!</p>Ef þér líkar og vilt styðja okkur geturðu lagt þitt af mörkum með því að hjálpa til við þýðingar, gefa o.s.frv. <a href="http://piwigo.org/basics/contribute" target="_blank">Click here to support us</a> Takk!';
+$lang['2_7_0_stp2'] = 'Eins og þú sérð hefur nýrri sjálfgefinni viðbót verið bætt við: Fáðu leiðsögn. Þessi viðbót hjálpar þér að uppgötva þitt Piwigo. Hér eru allar tiltækar leiðsagnir skráðar.<br>Þessi síða er fáanleg í <em>viðbætur » Fáður leiðsögn</em>';
+$lang['2_7_0_stp10'] = 'Sía fyrir skráarstærð hefur einnig verið bætt við lotustjórann. Þú getur valið svið með lágmarks- og hámarksgildi.';
+$lang['2_7_0_stp1'] = 'Halló! Ég mun vera leiðbeinandi þinn við að uppgötva Piwigo. Vinsamlegast fylgdu leiðbeiningunum mínum og smelltu á Next (eða notaðu örvarnar á lyklaborðinu þínu) til að fletta. Ef þú ferð á aðra síðu verður þér vísað á núverandi síðu leiðsagnarinnar.';
