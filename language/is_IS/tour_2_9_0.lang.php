fcfab45168deb9dba3aa0370ddeae025368a011a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-08-28 05:14:49 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/tour_2_9_0.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['2_9_0_stp1'] = 'Hér erum við komin með stærstu breytinguna á 2.9: hönnun! Dökkur haus, fótur og valmynd. Minni rammi.<br><br>Glæný stjórnun heimasíða, kölluð "Stjórnborð", með stórum táknum fyrir skjótan aðgang að aðalsíðum.';
+$lang['2_9_0_stp2'] = 'Útgáfa 2.9 fellur nú inn lykilorðagjafa beint á eyðublað til að búa til notendur.<br><br>Smelltu á þennan aðgerðartengil og sjáðu hvað gerist!';
+$lang['2_9_0_stp3'] = 'Þegar þú eyðir albúmi sem inniheldur myndir mun Piwigo spyrja þig núna hvað þú vilt gera við myndir sem tengjast albúminu.<br><br>Smelltu á eyðingartengilinn til að sjá þær (þú hættir við).';
+$lang['2_9_0_stp4'] = 'Sækja hlekk á myndvinnslusíðu, fyrir stjórnendur.';
+$lang['2_9_0_stp5'] = 'Finndu afrit myndir byggðar á athugunarsummu (nákvæm samsvörun á innihaldi skráar)';
+$lang['2_9_0_stp6'] = 'Sjálfgefið er að saga opnast nú í tölfræði yfirstandandi mánaðar.';
+$lang['2_9_0_stp7'] = 'Merkjastjórinn hefur nú sérstakan valham, til að beita aðgerðum í lotu.<br><br>Smelltu á hlekkinn til að skipta yfir í valham.';
+$lang['2_9_0_title1'] = 'Velkominn í Piwigo 2.9';
+$lang['2_9_0_title2'] = 'Lykilorðagjafi';
+$lang['2_9_0_title3'] = 'Valkostir við eyðingu albúms';
+$lang['2_9_0_title4'] = 'Hlaða niður mynd';
+$lang['2_9_0_title5'] = 'Afrit á tékksummu';
+$lang['2_9_0_title6'] = 'Saga núverandi mánaðar';
+$lang['2_9_0_title7'] = 'Merkjavalsstilling';
