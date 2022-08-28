019cef777d5a5e1bd537fafbccb48a18243afd7d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-08-28 05:14:53 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/tour_2_8_0.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['2_8_0_stp1'] = 'Notendaupplýsingar eru ekki lengur opnaðar inni í notendalistanum, þú færð einfalt eyðublað sem beinist að einum notanda.<br><br>Piwigo 2.8 lagar einnig nokkrar villur eins og uppfærslu notendalista þegar notandi hefur verið breytt eða ógilt netfang athugað.';
+$lang['2_8_0_stp2'] = 'Þegar tilkynnt er um albúm með tölvupósti, Piwigo 2.8 gefur þér vald til að velja notendur, ekki aðeins hópa.';
+$lang['2_8_0_stp3'] = 'Einfaldlega sagt, auðkenningarlyklar þýða að notendur þínir þurfa ekki lengur að muna lykilorðið sitt.<br><br>Þeir munu fá aðgang að einkaefninu þínu beint og örugglega.';
+$lang['2_8_0_stp4'] = 'Það er nú hægt að endurtaka vatnsmerkið á nokkrum línum.<br><br>Settu <em>Y endurtekningu</em> stillinguna á 1 eða 2 til að fá endurtekningu.';
+$lang['2_8_0_stp5'] = 'Þegar þú eyðir albúmi geta sumar myndir orðið munaðarlausar. Piwigo 2.8 gerir þær augljósar...';
+$lang['2_8_0_stp6'] = '... og lætur þig takast á við þá með einum smelli!';
+$lang['2_8_0_title1'] = 'Velkominn í Piwigo 2.8';
+$lang['2_8_0_title2'] = 'Láta notanda vita';
+$lang['2_8_0_title3'] = 'Auðkenningarlyklar';
+$lang['2_8_0_title4'] = 'Vatnsmerki';
+$lang['2_8_0_title5'] = 'Munaðarlausar';
+$lang['2_8_0_title6'] = 'Munaðarlausar';
